a6778329aaffcb9b4aba3e24d85d848b33f402ed
khfiii <khfiii@users.noreply.github.com>
2023-08-14 02:17:43 +0000

    chore: fix code style

--- app/UI/Panel/Widgets/ConferenceInformationWidget.php
+++ app/UI/Panel/Widgets/ConferenceInformationWidget.php
@@ -7,5 +7,6 @@ use Filament\Widgets\Widget;
 class ConferenceInformationWidget extends Widget
 {
     protected static string $view = 'u-i.panel.widgets.conference-information-widget';
-    protected int | string | array $columnSpan = 'full';
+
+    protected int|string|array $columnSpan = 'full';
 }
