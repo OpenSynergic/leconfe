8be9351b1b4b420dd813310ddbb7d85bfe41b2d9
Rahman Ramsi <rahmanramsi19@gmail.com>
2023-08-16 15:04:07 +0800

    apply necessary middleware

--- app/Providers/WebsiteServiceProvider.php
+++ app/Providers/WebsiteServiceProvider.php
@@ -2,9 +2,18 @@
 
 namespace App\Providers;
 
+use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\EncryptCookies;
+use App\Http\Middleware\VerifyCsrfToken;
 use App\Http\Middleware\Website\ApplyCurrentConference;
 use App\Website\Pages\Home;
+use Filament\Facades\Filament;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\AuthenticateSession;
+use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
 use Rahmanramsi\LivewirePageGroup\PageGroup;
 use Rahmanramsi\LivewirePageGroup\PageGroupServiceProvider;
 
@@ -18,6 +27,13 @@ class WebsiteServiceProvider extends PageGroupServiceProvider
             ->layout('website.layouts.app')
             ->homePage(Home::class)
             ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
                 ApplyCurrentConference::class,
             ], true)
             ->discoverPages(in: app_path('Website/Pages'), for: 'App\\Website\\Pages');
