a00f5cae2e1c3c1f47c2a42fd870f113696015ce
Dede Nugroho <this.nugroho@gmail.com>
2023-08-29 16:14:28 +0800

    Update class method

--- app/Facades/Block.php
+++ app/Facades/Block.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static void registerBlocks(array $blocks)
+ * @method static \Illuminate\Support\Collection getBlocks(string $position, bool $includeInactive = false)
  */
 class Block extends Facade
 {
