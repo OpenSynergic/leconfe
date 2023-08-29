7c5bb3a2d2094465f2838b5a3bf972955be18ce9
Rahman Ramsi <rahmanramsi19@gmail.com>
2023-08-29 16:47:53 +0800

    Example page for other developer

--- resources/views/conference/components/layouts/base.blade.php
+++ resources/views/conference/components/layouts/base.blade.php
@@ -8,7 +8,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $livewire?->getTitle() ?? '' }}</title>
+
+    <title>
+        {{ filled($title = strip_tags($livewire->getTitle())) ? "{$title} - " : null }}
+        {{ $currentConference->name }}
+    </title>
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
