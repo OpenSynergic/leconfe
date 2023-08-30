c42ed7a72ab30c810e0023d92c8f3c3477c2627c
Dede Nugroho <this.nugroho@gmail.com>
2023-08-30 21:44:24 +0800

    Fix: can not disable both sidebar

--- app/Models/Constants/SidebarPosition.php
+++ app/Models/Constants/SidebarPosition.php
@@ -7,4 +7,20 @@ class SidebarPosition
     const Left = 'left';
     const Right = 'right';
     const Both = 'both';
+    const None = "none";
+
+    public static function isRight()
+    {
+        return setting('sidebar') == static::Right || setting('sidebar') == static::Both;
+    }
+
+    public static function isLeft()
+    {
+        return setting('sidebar') == static::Left || setting('sidebar') == static::Both;
+    }
+
+    public static function isNone()
+    {
+        return setting('sidebar') == static::None;
+    }
 }
--- resources/views/conference/components/layouts/main.blade.php
+++ resources/views/conference/components/layouts/main.blade.php
@@ -1,7 +1,11 @@
 <div class="page-main mx-auto max-w-7xl flex flex-col lg:grid grid-cols-12 gap-3 grow w-full">
-    <x-conference::layouts.leftbar />
+    @if(\App\Models\Constants\SidebarPosition::isLeft())
+        <x-conference::layouts.leftbar />
+    @endif
     <div class="page-content">
         {{ $slot }}
     </div>
-    <x-conference::layouts.rightbar />
+    @if(\App\Models\Constants\SidebarPosition::isRight())
+        <x-conference::layouts.rightbar />
+    @endif
 </div>
