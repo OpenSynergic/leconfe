7296b7827b5ea833563067eac3b6b4e93d8323b1
Dede Nugroho <this.nugroho@gmail.com>
2023-08-30 21:25:40 +0800

    fix: wrong path

--- app/Website/Pages/Installation.php
+++ app/Website/Pages/Installation.php
@@ -44,7 +44,7 @@ class Installation extends Page
 
     public static function getLayout(): string
     {
-        return 'website.components.layouts.base';
+        return 'conference.components.layouts.base';
     }
 
     public function checkPermission()
@@ -62,21 +62,19 @@ class Installation extends Page
     public function stepDatabase()
     {
         $this->database->validate();
-        if (! $this->database->checkConnection()) {
+        if (!$this->database->checkConnection()) {
             return false;
         }
-
     }
 
     public function stepAccount()
     {
         $this->account->validate();
-
     }
 
     public function install()
     {
-        if (! $this->validateInstallation()) {
+        if (!$this->validateInstallation()) {
             return;
         }
 
@@ -99,7 +97,7 @@ class Installation extends Page
         $this->account->validate();
         $this->database->validate();
         $this->conference->validate();
-        if (! $this->database->checkConnection()) {
+        if (!$this->database->checkConnection()) {
             return false;
         }
 
