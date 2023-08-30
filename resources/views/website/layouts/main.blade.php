0b00a9c5373fda1f269b5295f7fe3e5de5375a35
Dede Nugroho <this.nugroho@gmail.com>
2023-08-30 15:04:26 +0800

    Implements sidebar configuration

--- resources/views/website/layouts/main.blade.php
+++ resources/views/website/layouts/main.blade.php
@@ -1,7 +1,11 @@
 <div class="page-main mx-auto max-w-7xl flex flex-col lg:grid grid-cols-12 gap-2 grow w-full">
-    @include('website.layouts.leftbar')
-    <div class="page-content">
+    @if(setting('sidebar') == \App\Models\Constants\SidebarPosition::Left || setting('sidebar') == \App\Models\Constants\SidebarPosition::Both)
+        @include('website.layouts.leftbar')
+    @endif
+    <div class="page-content @if(setting('sidebar') != \App\Models\Constants\SidebarPosition::Both) !col-span-8 @endif">
         {{ $slot }}
     </div>
-    @include('website.layouts.rightbar')
+    @if(setting('sidebar') == \App\Models\Constants\SidebarPosition::Right || setting('sidebar') == \App\Models\Constants\SidebarPosition::Both)
+        @include('website.layouts.rightbar')
+    @endif
 </div>
--- resources/views/website/layouts/rightbar.blade.php
+++ resources/views/website/layouts/rightbar.blade.php
@@ -1,16 +1,7 @@
 <div class="page-rightbar">
-    @foreach(Block::getBlocks(position: 'right') as $block)
-        @livewire($block) 
-    @endforeach
-    {{-- <div class="block space-y-2">
-        <h2 class="card-title">Dummy !</h2>
-        <div class="card card-compact bg-white border">
-            <div class="card-body">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum facere rem, laboriosam corporis temporibus possimus in quas perspiciatis voluptatem totam harum voluptate consequatur maxime quidem quaerat laudantium dolorum incidunt dolorem.</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-secondary btn-sm">Another Button</button>
-                </div>
-            </div>
-        </div>
-    </div> --}}
+    <div class="space-y-2">
+        @foreach(Block::getBlocks(position: 'right') as $block)
+            @livewire($block) 
+        @endforeach
+    </div>
 </div>
