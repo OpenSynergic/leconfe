80268cb62f68b6211c9659a471cff4d999bcbfbc
Dede Nugroho <this.nugroho@gmail.com>
2023-08-31 08:59:46 +0800

    Fixed

--- resources/views/conference/components/layouts/main.blade.php
+++ resources/views/conference/components/layouts/main.blade.php
@@ -1,11 +1,17 @@
+@php
+    $leftSidebarActive = \App\Models\Constants\SidebarPosition::isLeft();
+    $rightSidebarActive = \App\Models\Constants\SidebarPosition::isRight();
+    $bothSidebarActive = \App\Models\Constants\SidebarPosition::isBoth();
+    $sidebarActive = $leftSidebarActive || $rightSidebarActive;
+@endphp
 <div class="page-main mx-auto max-w-7xl flex flex-col lg:grid grid-cols-12 gap-3 grow w-full">
-    @if(\App\Models\Constants\SidebarPosition::isLeft())
+    @if($leftSidebarActive || $bothSidebarActive)
         <x-conference::layouts.leftbar />
     @endif
-    <div class="page-content">
+    <div class="page-content {{ $sidebarActive ? '!col-span-9' : ($bothSidebarActive ? '' : '!col-span-12') }}">
         {{ $slot }}
     </div>
-    @if(\App\Models\Constants\SidebarPosition::isRight())
+    @if($rightSidebarActive || $bothSidebarActive)
         <x-conference::layouts.rightbar />
     @endif
 </div>
