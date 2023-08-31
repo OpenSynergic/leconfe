1e960da280e27a890c10075ff923eb66ee1cb945
Dede Nugroho <this.nugroho@gmail.com>
2023-08-31 08:59:46 +0800

    Fixed

--- app/Models/Constants/SidebarPosition.php
+++ app/Models/Constants/SidebarPosition.php
@@ -11,12 +11,17 @@ class SidebarPosition
 
     public static function isRight()
     {
-        return setting('sidebar') == static::Right || setting('sidebar') == static::Both;
+        return setting('sidebar') == static::Right;
+    }
+
+    public static function isBoth()
+    {
+        return setting('sidebar') == static::Both;
     }
 
     public static function isLeft()
     {
-        return setting('sidebar') == static::Left || setting('sidebar') == static::Both;
+        return setting('sidebar') == static::Left;
     }
 
     public static function isNone()
