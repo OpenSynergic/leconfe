e28d0ac5076e427cd25c0d060f39a695f590e9d5
Dede Nugroho <this.nugroho@gmail.com>
2023-09-01 09:16:30 +0800

    $name is required and change class to abstract

--- app/Website/Blocks/ExampleBlock.php
+++ app/Website/Blocks/ExampleBlock.php
@@ -3,13 +3,14 @@
 namespace App\Website\Blocks;
 
 use App\Livewire\Block;
-use Illuminate\Contracts\View\View;
 
 class ExampleBlock extends Block
 {
     protected string | null $view = 'website.blocks.example-block';
 
     protected int | null $sort = 1;
+
+    protected string $name = "Example Block";
 
     protected string | null $position = 'right';
 }
