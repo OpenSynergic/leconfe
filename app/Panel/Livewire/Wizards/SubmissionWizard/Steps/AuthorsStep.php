becef2a0ef591b248ba882306b83f3467b44c9aa
Dede Nugroho <this.nugroho@gmail.com>
2023-09-18 14:47:11 +0800

    wip: author step

--- app/Panel/Livewire/Wizards/SubmissionWizard/Steps/AuthorsStep.php
+++ app/Panel/Livewire/Wizards/SubmissionWizard/Steps/AuthorsStep.php
@@ -2,43 +2,42 @@
 
 namespace App\Panel\Livewire\Wizards\SubmissionWizard\Steps;
 
-use App\Actions\Authors\AuthorCreateAction;
-use App\Actions\Authors\AuthorUpdateAction;
 use App\Actions\Participants\ParticipantCreateAction;
-use App\Actions\Submissions\SubmissionAddAuthorAction;
 use App\Actions\Submissions\SubmissionAddParticipant;
 use App\Actions\Submissions\SubmissionUpdateAction;
-use App\Models\Author;
 use App\Models\Participant;
 use App\Models\ParticipantPosition;
 use App\Models\Submission;
-use App\Models\SubmissionParticipant;
 use App\Panel\Livewire\Wizards\SubmissionWizard\Contracts\HasWizardStep;
 use App\Panel\Resources\Conferences\ParticipantResource;
 use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Actions\ActionGroup;
 use Filament\Tables\Actions\CreateAction;
-use Filament\Tables\Actions\DeleteAction;
-use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Columns\Layout\Split;
 use Filament\Tables\Columns\Layout\Stack;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Table;
 use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Facades\DB;
 use Livewire\Component;
-use Squire\Models\Country;
 
 class AuthorsStep extends Component implements HasForms, HasTable, HasWizardStep
 {
     use InteractsWithForms, InteractsWithTable;
 
     public Submission $record;
+
+    protected function getTableHeading(): string | Htmlable | null
+    {
+        return "Authors";
+    }
 
     public static function getWizardLabel(): string
     {
@@ -50,10 +49,97 @@ class AuthorsStep extends Component implements HasForms, HasTable, HasWizardStep
         return view('panel.livewire.wizards.submission-wizard.steps.authors-step');
     }
 
+    public function table(Table $table)
+    {
+        return $table
+            ->query($this->getTableQuery())
+            ->heading("Authors")
+            ->headerActions([
+                ActionGroup::make([
+                    CreateAction::make()
+                        ->label('Create new')
+                        ->modalWidth('2xl')
+                        ->modalHeading("Add Author")
+                        ->form($this->getAuthorFormSchema())
+                        ->using(function (array $data) {
+                            $participant = ParticipantCreateAction::run($data);
+                            $positionAuthor = ParticipantPosition::find($data['type']);
+                            SubmissionAddParticipant::run($this->record, $participant, $positionAuthor);
+                            return $participant;
+                        }),
+                    Action::make('add_existing')
+                        ->label("Add from existing")
+                        ->modalWidth("lg")
+                        ->form([
+                            Grid::make()
+                                ->schema([
+                                    Select::make('participant_id')
+                                        ->label("Name")
+                                        ->options(function () {
+                                            return $this->getTableQuery(submissionRelated: false)
+                                                ->get()
+                                                ->pluck('fullName', 'id')
+                                                ->toArray();
+                                        })
+                                        ->noSearchResultsMessage('No authors found')
+                                        ->preload()
+                                        ->required()
+                                        ->columnSpanFull(),
+                                    Select::make('type')
+                                        ->relationship(
+                                            name: 'positions',
+                                            titleAttribute: 'name',
+                                            modifyQueryUsing: fn (Builder $query) => $query->whereIn('type', ['author', 'speaker'])
+                                                ->groupBy('name')
+                                        )
+                                        ->preload()
+                                        ->required()
+                                        ->columnSpanFull(),
+                                ])
+                        ])
+                        ->action(function (array $data) {
+                            dd($data);
+                        })
+                ])
+                    ->button()
+                    ->label("Add author")
+            ])
+            ->columns([
+                Split::make([
+                    Stack::make([
+                        TextColumn::make('fullName')
+                            ->size('sm')
+                            ->wrap()
+                            // ->searchable(query: function (Builder $query, string $search): Builder {
+                            //     return $query
+                            //         ->whereMeta('public_name', 'like', "%{$search}%")
+                            //         ->orWhere(fn (Builder $query) => $query->whereMeta('family_name', 'like', "%{$search}%"))
+                            //         ->orWhere(fn (Builder $query) => $query->whereMeta('given_name', 'like', "%{$search}%"));
+                            // })
+                            ->weight('bold'),
+                        TextColumn::make('affiliation')
+                            ->getStateUsing(fn ($record) => $record->getMeta('affiliation'))
+                            ->extraAttributes([
+                                'class' => 'text-xs',
+                            ])
+                            ->color('gray'),
+                    ]),
+                    TextColumn::make('email')
+                        ->extraAttributes([
+                            'class' => 'text-xs',
+                        ])
+                        ->color('gray')
+                        ->icon('heroicon-o-envelope')
+                        ->alignStart(),
+                ])->from('lg'),
+            ]);
+    }
+
     public function nextStep()
     {
-        if ($this->getTableQuery()->count() < 1) {
-            return session()->flash('no_authors', 'No authors were added to the submission');
+        if ($this->getTableQuery()->count()) {
+            $this->addError('errors', 'You must add at least one author');
+            return;
         }
 
         SubmissionUpdateAction::run([
@@ -64,7 +150,7 @@ class AuthorsStep extends Component implements HasForms, HasTable, HasWizardStep
         $this->dispatch('refreshLivewire');
     }
 
-    protected function getTableQuery(): Builder
+    protected function getTableQuery(bool $submissionRelated = true): Builder
     {
         return Participant::query()->with([
             'positions' => fn ($query) => $query
@@ -76,9 +162,13 @@ class AuthorsStep extends Component implements HasForms, HasTable, HasWizardStep
                 'positions',
                 fn (Builder $query) => $query->where('type', 'author')
             )
-            ->whereIn(
-                'id',
-                $this->record->participants()->pluck('participant_id')
+            ->when(
+                $submissionRelated,
+                fn (Builder $query) => $query->whereIn('id', $this->record->participants()->pluck('participant_id'))
+            )
+            ->when(
+                !$submissionRelated,
+                fn (Builder $query) => $query->whereNotIn('id', $this->record->participants()->pluck('participant_id'))
             );
     }
 
@@ -107,55 +197,6 @@ class AuthorsStep extends Component implements HasForms, HasTable, HasWizardStep
                 ])->columnSpan([
                     'lg' => 2,
                 ]),
-        ];
-    }
-
-    protected function getTableHeaderActions(): array
-    {
-        return [
-            CreateAction::make()
-                ->label('Add Author')
-                ->modalWidth('2xl')
-                ->modalHeading("Add Author")
-                ->form($this->getAuthorFormSchema())
-                ->using(function (array $data) {
-                    $participant = ParticipantCreateAction::run($data);
-                    $positionAuthor = ParticipantPosition::find($data['type']);
-                    SubmissionAddParticipant::run($this->record, $participant, $positionAuthor);
-                })
-        ];
-    }
-
-    protected function getTableColumns(): array
-    {
-        return [
-            Split::make([
-                Stack::make([
-                    TextColumn::make('fullName')
-                        ->size('sm')
-                        ->wrap()
-                        // ->searchable(query: function (Builder $query, string $search): Builder {
-                        //     return $query
-                        //         ->whereMeta('public_name', 'like', "%{$search}%")
-                        //         ->orWhere(fn (Builder $query) => $query->whereMeta('family_name', 'like', "%{$search}%"))
-                        //         ->orWhere(fn (Builder $query) => $query->whereMeta('given_name', 'like', "%{$search}%"));
-                        // })
-                        ->weight('bold'),
-                    TextColumn::make('affiliation')
-                        ->getStateUsing(fn ($record) => $record->getMeta('affiliation'))
-                        ->extraAttributes([
-                            'class' => 'text-xs',
-                        ])
-                        ->color('gray'),
-                ]),
-                TextColumn::make('email')
-                    ->extraAttributes([
-                        'class' => 'text-xs',
-                    ])
-                    ->color('gray')
-                    ->icon('heroicon-o-envelope')
-                    ->alignStart(),
-            ])->from('lg'),
         ];
     }
 
