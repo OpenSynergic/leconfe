d9a95da0bce5d9cc3ae9fd0448dc3d824ebec7bc
Dede Nugroho <this.nugroho@gmail.com>
2023-09-18 09:42:56 +0800

    Fix

--- resources/views/conference/components/layouts/main.blade.php
+++ resources/views/conference/components/layouts/main.blade.php
@@ -1,16 +1,17 @@
-
 @php
-    if(!function_exists('isSidebar')) {
-        // If $currentConference is inside a function, it is undefined.
-        function isSidebar($currentConference, $position): bool {
-            return $currentConference->getMeta('sidebar') == $position->getValue();
-        }
+    use \App\Models\Enums\SidebarPosition;
+    $leftSidebarActive = $rightSidebarActive = $bothSidebarActive = $sidebarActive = false;
+    
+    if (isset($currentConference)) {
+        $sidebarActive = isSidebar($currentConference, SidebarPosition::Left) || isSidebar($currentConference, SidebarPosition::Right);
+        $bothSidebarActive = isSidebar($currentConference, SidebarPosition::Both);
     }
-    $leftSidebarActive = isSidebar($currentConference, \App\Models\Enums\SidebarPosition::Left);
-    $rightSidebarActive = isSidebar($currentConference, \App\Models\Enums\SidebarPosition::Right);
-    $bothSidebarActive = isSidebar($currentConference, \App\Models\Enums\SidebarPosition::Both);
-    $sidebarActive = $leftSidebarActive || $rightSidebarActive;
+    
+    function isSidebar($currentConference, $position): bool {
+        return $currentConference->getMeta('sidebar') == $position->getValue();
+    }
 @endphp
+
 <div class="page-main mx-auto max-w-7xl flex flex-col lg:grid grid-cols-12 gap-3 grow w-full">
     @if($leftSidebarActive || $bothSidebarActive)
         <x-conference::layouts.leftbar />
