92d59daf952a359ed39bcb5a1e60b1b27a6be0e2
Dede Nugroho <this.nugroho@gmail.com>
2023-09-19 10:34:44 +0800

    Fix styling

--- resources/views/panel/livewire/wizards/submission-wizard/steps/detail-step.blade.php
+++ resources/views/panel/livewire/wizards/submission-wizard/steps/detail-step.blade.php
@@ -1,19 +1,20 @@
 <form wire:submit="submit">
-    {{ $this->form }}
-    <div class="flex items-center justify-between mt-4">
-        <div>
-            <x-filament::button icon="heroicon-o-chevron-left" x-show="! isFirstStep()" x-cloak x-on:click="previousStep"
-                color="secondary" size="sm">
-                Previous
-            </x-filament::button>
-            
-        </div>
-
-        <div>
-            <x-filament::button type="submit" icon="heroicon-o-chevron-right" icon-position="after" x-show="! isLastStep()" x-cloak
-                wire:loading.class.delay="opacity-70 cursor-wait" size="sm">
-                Next
-            </x-filament::button>
+    <div class="space-y-6">
+        {{ $this->form }}
+        <div class="flex items-center justify-between mt-4">
+            <div>
+                <x-filament::button icon="heroicon-o-chevron-left" x-show="! isFirstStep()" x-cloak x-on:click="previousStep"
+                    color="secondary" size="sm">
+                    Previous
+                </x-filament::button>
+            </div>
+    
+            <div>
+                <x-filament::button type="submit" icon="heroicon-o-chevron-right" icon-position="after" x-show="! isLastStep()" x-cloak
+                    wire:loading.class.delay="opacity-70 cursor-wait" size="sm">
+                    Next
+                </x-filament::button>
+            </div>
         </div>
     </div>
 </form>
