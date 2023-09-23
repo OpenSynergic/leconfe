df46ed0f5ebaff9ab09bcd63df855c9fddf5e725
Rahman Ramsi <rahmanramsi19@gmail.com>
2023-09-23 08:34:42 +0800

    wip

--- app/Models/Navigation.php
+++ app/Models/Navigation.php
@@ -9,4 +9,14 @@ use RyanChandler\FilamentNavigation\Models\Navigation as Model;
 class Navigation extends Model
 {
     use BelongsToConference, Cachable;
+
+    /**
+     * The "booted" method of the model.
+     */
+    protected static function booted(): void
+    {
+        static::creating(function (Navigation $navigation) {
+            $navigation->conference_id ??= 0;
+        });
+    }
 }
