b58e1013074753d8b4d47648609d6c22d8bae93d
Rahman Ramsi <rahmanramsi19@gmail.com>
2023-09-26 11:31:32 +0800

    cache block

--- app/Models/Block.php
+++ app/Models/Block.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use App\Models\Concerns\BelongsToConference;
+use GeneaLabs\LaravelModelCaching\Traits\Cachable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Block extends Model
 {
-    use BelongsToConference, HasFactory;
+    use BelongsToConference, HasFactory, Cachable;
 
     protected $fillable = [
         'class',
