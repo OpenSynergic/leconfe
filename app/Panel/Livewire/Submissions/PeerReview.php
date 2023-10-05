fbfb2ea1f8a448ba03e4557a90b5353e48ff2850
Dede Nugroho <this.nugroho@gmail.com>
2023-10-05 11:43:07 +0800

    WIP: Peer Reeview Stage

--- app/Panel/Livewire/Submissions/PeerReview.php
+++ app/Panel/Livewire/Submissions/PeerReview.php
@@ -2,54 +2,28 @@
 
 namespace App\Panel\Livewire\Submissions;
 
-use App\Infolists\Components\LivewireEntry;
 use App\Models\Submission;
-use App\Panel\Livewire\Submissions\SubmissionDetail\Discussions;
-use App\Panel\Livewire\Tables\Submissions\SubmissionFilesTable;
+use App\Panel\Livewire\Workflows\Concerns\InteractWithTenant;
 use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Actions\Contracts\HasActions;
-use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
-use Filament\Infolists\Concerns\InteractsWithInfolists;
-use Filament\Infolists\Contracts\HasInfolists;
-use Filament\Infolists\Infolist;
-use Filament\Tables\Actions\Action;
-use Filament\Tables\Concerns\InteractsWithTable;
-use Filament\Tables\Contracts\HasTable;
-use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component;
 
-class PeerReview extends Component implements HasForms, HasActions, HasTable
+class PeerReview extends Component implements HasForms, HasActions
 {
     use InteractsWithForms;
-    use InteractsWithTable;
     use InteractsWithActions;
+    use InteractWithTenant;
 
     public Submission $submission;
+
+    public bool $stageOpened = false;
 
     public function mount(Submission $submission)
     {
         $this->submission = $submission;
-    }
-
-    public function table(Table $table): Table
-    {
-        return $table
-            ->query(fn () => $this->submission->papers())
-            ->heading("Papers")
-            ->headerActions([
-                Action::make('download-template')
-                    ->label("Download Template")
-                    ->color("gray"),
-                Action::make('upload-paper')
-                    ->modalWidth("xl")
-                    ->form([
-                        SpatieMediaLibraryFileUpload::make('submission-papers')
-                    ])
-            ])
-            ->columns([]);
+        $this->stageOpened = $this->conference->getMeta("workflow.peer-review.open", false);
     }
 
     public function render()
--- resources/views/panel/livewire/submissions/peer-review.blade.php
+++ resources/views/panel/livewire/submissions/peer-review.blade.php
@@ -1,3 +1,18 @@
 <div class="space-y-6">
-  {{ $this->table }}
+  @if($stageOpened)
+    <div class="grid grid-cols-12 gap-4">
+      <div class="space-y-4 col-span-8">
+          @livewire(App\Panel\Livewire\Tables\Submissions\SubmissionFilesTable::class, ['record' => $submission, 'category' => 'submission-papers'])
+          @livewire(App\Panel\Livewire\Submissions\Components\ReviewerList::class, ['record' => $submission])
+          @livewire(App\Panel\Livewire\Submissions\SubmissionDetail\Discussions::class, ['record' => $submission])
+      </div>
+      <div class="self-start sticky top-24 flex flex-col gap-3 col-span-4">
+
+      </div>
+    </div>
+  @else
+    <div class="bg-warning-700 p-4 rounded-lg text-base">
+      The stage has not yet been opened.
+    </div>
+  @endif
 </div>
