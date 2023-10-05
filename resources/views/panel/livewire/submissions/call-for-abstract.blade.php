3d8fb4bf51534902314e40a98a650aef8bca3a93
Dede Nugroho <this.nugroho@gmail.com>
2023-10-05 11:40:42 +0800

    Fix layout.

--- resources/views/panel/livewire/submissions/call-for-abstract.blade.php
+++ resources/views/panel/livewire/submissions/call-for-abstract.blade.php
@@ -1,25 +1,25 @@
 <div class="space-y-6">
-  <div class="flex gap-4">
-    <div class="space-y-4">
-      @livewire(App\Panel\Livewire\Tables\Submissions\SubmissionFilesTable::class, ['record' => $submission, 'category' => 'submission-files'])
-      @livewire(App\Panel\Livewire\Submissions\SubmissionDetail\Discussions::class, ['submission' => $submission])
-    </div>
-      <div class="w-3/6 self-start sticky top-24 flex flex-col gap-3">
-        @if($submission->accepted())
-          <div class="bg-primary-700 p-4 rounded-lg text-base">
-            Your submission has been accepted. Now, we are waiting to next stage is open.
-          </div>
-        @endif
-        <div id="callforabstract-action" class="space-y-4">
-          @if(! $submission->accepted())
-            {{ $this->acceptAction() }}
-            {{ $this->declineAction() }}
-          @endif
+    <div class="grid grid-cols-12 gap-4">
+        <div class="space-y-4 col-span-8">
+            @livewire(App\Panel\Livewire\Tables\Submissions\SubmissionFilesTable::class, ['record' => $submission, 'category' => 'submission-files'])
+            @livewire(App\Panel\Livewire\Submissions\SubmissionDetail\Discussions::class, ['record' => $submission])
         </div>
-        <div id="participants">
-          @livewire(App\Panel\Livewire\Submissions\SubmissionDetail\AssignParticipants::class, ['submission' => $submission])
+        <div class="self-start sticky top-24 flex flex-col gap-3 col-span-4">
+            @if ($submission->accepted())
+                <div class="bg-primary-700 p-4 rounded-lg text-base">
+                    Your submission has been accepted. Now, we are waiting to next stage is open.
+                </div>
+            @endif
+            <div id="callforabstract-action" class="space-y-4">
+                @if (!$submission->accepted())
+                    {{ $this->acceptAction() }}
+                    {{ $this->declineAction() }}
+                @endif
+            </div>
+            <div id="participants">
+                @livewire(App\Panel\Livewire\Submissions\SubmissionDetail\AssignParticipants::class, ['submission' => $submission])
+            </div>
         </div>
     </div>
-  </div>
-  <x-filament-actions::modals />
+    <x-filament-actions::modals />
 </div>
