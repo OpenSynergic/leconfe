51a738112061047232b33a44cb9b1cf250ebb9fa
Dede Nugroho <this.nugroho@gmail.com>
2023-10-05 16:26:13 +0800

    WIP: Reviewer list

--- app/Panel/Livewire/Submissions/Components/ReviewerList.php
+++ app/Panel/Livewire/Submissions/Components/ReviewerList.php
@@ -3,10 +3,20 @@
 namespace App\Panel\Livewire\Submissions\Components;
 
 use App\Models\Enums\UserRole;
+use App\Models\Participant;
 use App\Models\ParticipantPosition;
 use App\Models\Submission;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Placeholder;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\View;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
 use Filament\Tables\Actions\Action;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
@@ -21,6 +31,11 @@ class ReviewerList extends Component implements HasForms, HasTable
 
     public Submission $record;
 
+    public static function renderSelectParticipant(Participant $participant): string
+    {
+        return view('forms.select-participant', ['participant' => $participant])->render();
+    }
+
     public function table(Table $table): Table
     {
         return $table
@@ -28,11 +43,62 @@ class ReviewerList extends Component implements HasForms, HasTable
                 $reviewerPosition = ParticipantPosition::where('name', UserRole::Reviewer->value)->first();
                 return $this->record->participants()->where('participant_position_id', $reviewerPosition->getKey());
             })
-            ->heading("Reviewer List")
+            ->heading("Reviewers")
             ->headerActions([
                 Action::make('add-reviewer')
                     ->label("Reviewer")
-                    ->form([])
+                    ->modalHeading("Assign Reviewer")
+                    ->modalWidth("2xl")
+                    ->form([
+                        Select::make('participant_id')
+                            ->label("Reviewer")
+                            ->placeholder("Select a reviewer")
+                            ->allowHtml()
+                            ->preload()
+                            ->required()
+                            ->searchable()
+                            ->options(function (): array {
+                                return Participant::with('positions')
+                                    ->whereHas('positions', function ($query) {
+                                        $query->where('name', UserRole::Reviewer->value);
+                                    })
+                                    ->get()
+                                    ->mapWithKeys(function (Participant $participant) {
+                                        return [$participant->getKey() => static::renderSelectParticipant($participant)];
+                                    })
+                                    ->toArray();
+                            }),
+                        Grid::make(2)
+                            ->hidden(fn (Get $get): bool => !$get('participant_id'))
+                            ->schema([
+                                Fieldset::make("Reviewer Statistic")
+                                    ->reactive()
+                                    ->schema(function (Get $get): array {
+                                        $participant = Participant::find($get('participant_id'));
+                                        if (!$participant) {
+                                            return [];
+                                        }
+                                        return [
+                                            Placeholder::make('Active reviews currently assigned')
+                                                ->content(0),
+                                            Placeholder::make('Reviews completed')
+                                                ->content(0),
+                                            Placeholder::make('Review requests declined')
+                                                ->content(0),
+                                            Placeholder::make('Average days to complete review')
+                                                ->content(0),
+                                        ];
+                                    }),
+                                Checkbox::make('send-reviewer-invitation')
+                                    ->reactive()
+                                    ->label("Send Reviewer Invitation")
+                                    ->columnSpanFull(),
+                                RichEditor::make("reviewer-invitation-message")
+                                    ->visible(fn (Get $get): bool => $get('send-reviewer-invitation'))
+                                    ->label("Reviewer invitation message")
+                                    ->columnSpanFull(),
+                            ])
+                    ])
             ])
             ->columns([]);
     }
