d6593fba24cc1a2ee346635933d69c97424538cb
Dede Nugroho <49790011+thisnugroho@users.noreply.github.com>
2023-10-06 10:59:36 +0800

    Add lazy load

--- resources/views/panel/livewire/submissions/call-for-abstract.blade.php
+++ resources/views/panel/livewire/submissions/call-for-abstract.blade.php
@@ -1,8 +1,8 @@
 <div class="space-y-6">
     <div class="grid grid-cols-12 gap-4">
         <div class="space-y-4 col-span-8">
-            @livewire(App\Panel\Livewire\Tables\Submissions\SubmissionFilesTable::class, ['record' => $submission, 'category' => 'submission-files'])
-            @livewire(App\Panel\Livewire\Submissions\SubmissionDetail\Discussions::class, ['record' => $submission])
+            @livewire(App\Panel\Livewire\Tables\Submissions\SubmissionFilesTable::class, ['record' => $submission, 'category' => 'submission-files', 'lazy' => true])
+            @livewire(App\Panel\Livewire\Submissions\SubmissionDetail\Discussions::class, ['record' => $submission, 'lazy' => true])
         </div>
         <div class="self-start sticky top-24 flex flex-col gap-3 col-span-4">
             @if ($submission->accepted())
