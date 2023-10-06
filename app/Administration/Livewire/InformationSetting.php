cdf0a4de8a370f2c3fccfd82b68db573b6672f77
Rahman Ramsi <rahmanramsi19@gmail.com>
2023-10-06 09:29:41 +0800

    InformationSetting on their own component

---
+++ app/Administration/Livewire/InformationSetting.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Administration\Livewire;
+
+use App\Actions\Site\SiteUpdateAction;
+use Filament\Forms\Components\Actions;
+use Filament\Forms\Components\Actions\Action;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Form;
+use Livewire\Component;
+
+class InformationSetting extends Component implements HasForms
+{
+    use InteractsWithForms;
+
+    public ?array $formData = [];
+
+    public function mount()
+    {
+        $this->form->fill([
+            'meta' => app()->getSite()->getAllMeta()->toArray(),
+        ]);
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make()
+                    ->schema([
+                        TextInput::make('meta.name')
+                            ->label('Website Name')
+                            ->required(),
+                        SpatieMediaLibraryFileUpload::make('logo')
+                            ->collection('logo')
+                            ->image()
+                            ->model(app()->getSite())
+                            ->imageResizeUpscale(false)
+                            ->conversion('thumb')
+                            ->columnSpan([
+                                'sm' => 2,
+                            ]),
+
+                    ])
+                    ->columns(2),
+                Actions::make([
+                    Action::make('save')
+                        ->successNotificationTitle('Saved!')
+                        ->failureNotificationTitle('Failed!')
+                        ->action(function (Action $action) {
+                            $data = $this->form->getState();
+                            try {
+                                SiteUpdateAction::run($data);
+
+                                $action->sendSuccessNotification();
+                            } catch (\Throwable $th) {
+                                $action->sendFailureNotification();
+                            }
+                        }),
+                ]),
+            ])
+            ->statePath('formData');
+    }
+
+    public function render()
+    {
+        return view('administration.livewire.information-setting');
+    }
+}
