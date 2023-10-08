8928583d633d580ee1ae11023752edcf3039b203
Dede Nugroho <49790011+thisnugroho@users.noreply.github.com>
2023-10-08 08:34:32 +0800

    fix HTML

--- resources/views/panel/resources/submission-resource/pages/view-submission.blade.php
+++ resources/views/panel/resources/submission-resource/pages/view-submission.blade.php
@@ -2,8 +2,6 @@
     @if ($this->record->status == App\Models\Enums\SubmissionStatus::Wizard)
         @livewire(App\Panel\Livewire\Wizards\SubmissionWizard::class, ['record' => $record])
     @else
-    <div class="flex">
         {{ $this->infolist }}
-    </div>
     @endif
 </x-filament::page>
