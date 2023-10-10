26b6e0b0c9750c2ff75870f99990e8cdb3a4ba13
Rahman Ramsi <rahmanramsi19@gmail.com>
2023-10-10 09:07:42 +0800

    fix static page doesnt show content

--- resources/views/website/pages/static-page.blade.php
+++ resources/views/website/pages/static-page.blade.php
@@ -1,11 +1,10 @@
 <x-website::layouts.main>
         <div class="card-body text-gray-800">
-            <p class="text-xs text-gray-500 font-medium">{{ $this->staticPage->created_at->format('l, j F Y') }}</p>
             <h1 class="card-title">{{ $this->staticPage->title }}</h1>
             <div 
                 class="user-content"
             >
-                {{ new Illuminate\Support\HtmlString($this->staticPage->getMeta('user_content')) }}
+                {{ new Illuminate\Support\HtmlString($this->staticPage->getMeta('content')) }}
             </div>
         </div>
 </x-conference::layouts.main>
