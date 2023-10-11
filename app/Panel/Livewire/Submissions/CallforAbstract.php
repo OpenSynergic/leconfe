4b5f52a381aa6cff5d7d484a570d4ca29231c6aa
Dede Nugroho <49790011+thisnugroho@users.noreply.github.com>
2023-10-11 15:03:51 +0800

    add: notification when accepting submission

--- app/Panel/Livewire/Submissions/CallforAbstract.php
+++ app/Panel/Livewire/Submissions/CallforAbstract.php
@@ -4,14 +4,20 @@ namespace App\Panel\Livewire\Submissions;
 
 use App\Actions\Submissions\SubmissionUpdateAction;
 use App\Infolists\Components\LivewireEntry;
+use App\Models\Enums\SubmissionStage;
 use App\Models\Enums\SubmissionStatus;
 use App\Models\Submission;
 use App\Panel\Livewire\Submissions\SubmissionDetail\Discussions;
 use Filament\Actions\Action;
 use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Actions\Contracts\HasActions;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Get;
 use Filament\Infolists\Concerns\InteractsWithInfolists;
 use Filament\Infolists\Contracts\HasInfolists;
 use Filament\Infolists\Infolist;
@@ -30,32 +36,89 @@ class CallforAbstract extends Component implements HasForms, HasActions
         return Action::make('decline')
             ->outlined()
             ->color("danger")
+            ->modalWidth("2xl")
+            ->record($this->submission)
+            ->modalHeading("Confirmation")
+            ->modalSubmitActionLabel("Decline")
             ->extraAttributes(['class' => 'w-full'], true)
-            ->icon("lineawesome-times-circle-solid")
-            ->requiresConfirmation();
+            ->form([
+                Fieldset::make("Notification")
+                    ->columns(1)
+                    ->schema([
+                        Checkbox::make('no-notification')
+                            ->label("Don't send notification to author")
+                            ->reactive()
+                            ->default(false),
+                        TextInput::make('email')
+                            ->hidden(fn (Get $get): bool => $get('no-notification'))
+                            ->disabled()
+                            ->formatStateUsing(fn (Submission $record): string => $record->user->email),
+                        RichEditor::make('message')
+                            ->hidden(fn (Get $get): bool => $get('no-notification'))
+                            ->disableToolbarButtons([
+                                'assignFiles'
+                            ])
+                    ])
+            ])
+            ->icon("lineawesome-times-circle-solid");
     }
 
     public function acceptAction()
     {
         return Action::make('accept')
+            ->modalHeading("Confirmation")
+            ->modalSubmitActionLabel("Accept")
+            ->modalWidth("2xl")
             ->outlined()
             ->record($this->submission)
             ->successNotificationTitle("Accepted")
             ->extraAttributes(['class' => 'w-full'])
             ->icon("lineawesome-check-circle-solid")
+            ->form([
+                Fieldset::make("Notification")
+                    ->columns(1)
+                    ->schema([
+                        /**
+                         * TODO:
+                         * - We need to create a function for it because it is used frequently.
+                         * 
+                         * Something like:
+                         *   UserNotificaiton::formSchema()
+                         */
+                        Checkbox::make('no-notification')
+                            ->label("Don't send notification to author")
+                            ->reactive()
+                            ->default(false),
+                        TextInput::make('email')
+                            ->hidden(fn (Get $get): bool => $get('no-notification'))
+                            ->disabled()
+                            ->formatStateUsing(fn (Submission $record): string => $record->user->email),
+                        RichEditor::make('message')
+                            ->hidden(fn (Get $get): bool => $get('no-notification'))
+                            ->disableToolbarButtons([
+                                'assignFiles'
+                            ])
+                    ])
+            ])
             ->action(function (Action $action) {
                 SubmissionUpdateAction::run([
-                    'status' => SubmissionStatus::Accepted
+                    'stage' => SubmissionStage::PeerReview,
+                    'status' => SubmissionStatus::OnReview
                 ], $this->submission);
+                /**
+                 * TODO: 
+                 * -  Send notificaion
+                 */
                 $this->dispatch("refreshPeerReview");
                 $action->success();
-            })
-            ->requiresConfirmation();
+            });
     }
 
 
     public function render()
     {
-        return view('panel.livewire.submissions.call-for-abstract');
+        return view('panel.livewire.submissions.call-for-abstract', [
+            'reviewStageOpen' => app()->getCurrentConference()->getMeta("workflow.peer-review.open", false),
+        ]);
     }
 }
