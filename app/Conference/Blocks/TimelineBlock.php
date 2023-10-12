36308cb5d70072fec50b6eae242ae76b2fa85cfd
khfiii <khfii635@gmail.com>
2023-10-12 16:59:17 +0800

    Business logic in right place

--- app/Conference/Blocks/TimelineBlock.php
+++ app/Conference/Blocks/TimelineBlock.php
@@ -9,13 +9,9 @@ use App\Models\Timeline;
 class TimelineBlock extends Block
 {
     protected ?string $view = 'conference.blocks.timeline-block';
-
     protected ?int $sort = 2;
-
     protected string $name = 'Timeline Block';
-
     protected ?string $position = 'left';
-
 
     public function getViewData(): array
     {
@@ -23,7 +19,6 @@ class TimelineBlock extends Block
 
         $timelines = Timeline::where('conference_id', app()->getCurrentConference()?->getKey())
             ->where(function ($query) use ($today) {
-                // 1 day before now (yesterday)
                 $query->where('date', $today->subDay()->toDateString())
                     ->orWhereBetween('date', [
                         $today->addDays(1)->toDateString(), // Today
@@ -31,14 +26,58 @@ class TimelineBlock extends Block
                     ]);
             })
             ->orWhere(function ($query) {
-                // If no data matches the above criteria, display only the latest 3 data
                 $query->latest()->limit(3);
             })
             ->orderBy('date')
             ->get();
 
+        $timelineData = [];
+
+        foreach ($timelines as $timeline) {
+            $background = match (true) {
+                $timeline->date <= now()->subDay() => 'past-timeline',
+                $timeline->date->isToday() => 'current-timeline',
+                default => 'upcoming-timeline',
+            };
+
+            $marker = match (true) {
+                $timeline->date <= now()->subDay() => 'past-marker',
+                $timeline->date->isToday() => 'current-marker',
+                default => 'upcoming-marker',
+            };
+
+            $badgeRoles = [];
+
+            $countRole = count($timeline->roles);
+
+            foreach ($timeline->roles as $key => $role) {
+                $badgeRole = match ($role) {
+                    'Author' => 'author-badge',
+                    'Editor' => 'editor-badge',
+                    'Reviewer' => 'reviewer-badge',
+                    'Participant' => 'participant-badge',
+                    default => 'participant-badge',
+                };
+
+                if ($key < 1) {
+                    $badgeRoles[] = [
+                        'badgeRole' => $badgeRole,
+                        'role' => $role,
+                        'moreCount' => $countRole - 1,
+                    ];
+                }
+            }
+
+            $timelineData[] = [
+                'timeline' => $timeline,
+                'timelineBackground' => $background,
+                'timelineMarker' => $marker,
+                'badgeRoles' => $badgeRoles,
+            ];
+        }
+
         return [
-            'timelines' => $timelines,
+            'timelines' => $timelineData,
         ];
     }
 }
--- resources/views/conference/blocks/timeline-block.blade.php
+++ resources/views/conference/blocks/timeline-block.blade.php
@@ -2,66 +2,28 @@
     @if (count($timelines) > 0)
         <h2 class="text-heading px-2 mb-1">Informations</h2>
         @foreach ($timelines as $timeline)
-            @php
-                // initialize
-                $timelineBackground = '';
-                $timelineMarker = '';
-
-                // Check if it's yesterday
-            if ($timeline->date <= now()->subDay()) {
-                // Set background and marker for past timeline
-                $timelineBackground = 'past-timeline';
-                $timelineMarker = 'past-marker';
-            }
-            // Check if it's today
-                elseif ($timeline->date->isToday()) {
-                    // Set background and marker for current timeline
-                    $timelineBackground = 'current-timeline';
-                    $timelineMarker = 'current-marker';
-                }
-                // It's not yesterday or today, so it's upcoming
-                else {
-                    // Set background and marker for upcoming timeline
-                    $timelineBackground = 'upcoming-timeline';
-                    $timelineMarker = 'upcoming-marker';
-                }
-            @endphp
-            <div class="{{ $timelineBackground }}">
+            <div class="{{ $timeline['timelineBackground'] }}">
                 <div class="w-full flex justify-between">
                     <div class="inline-flex items-center gap-2">
-                        <div class="{{ $timelineMarker }}"></div>
-                        <time class="text-xs">{{ date(setting('format.date'), strtotime($timeline->date)) }}</time>
+                        <div class="{{ $timeline['timelineMarker'] }}"></div>
+                        <time class="text-xs">{{ date(setting('format.date'), strtotime($timeline['timeline']->date)) }}</time>
                     </div>
                     <div class="flex flex-wrap gap-1">
-                        @foreach ($timeline->roles as $key => $role)
-                            @php
-                                $countRole = count($timeline->roles);
-                                $badgeRole = '';
-                                $badgeRole = match ($role) {
-                                    'Author' => 'author-badge',
-                                    'Editor' => 'editor-badge',
-                                    'Reviewer' => 'reviewer-badge',
-                                    'Participant' => 'participant-badge',
-                                    default => 'participant-badge',
-                                };
-
-                            @endphp
-                            @if ($key < 1)
-                                <div class="inline-flex gap-1">
-                                    <span class="{{ $badgeRole }}">
-                                        {{ $role }}
-                                    </span>
-                                    @if (count($timeline->roles) > 1)
-                                        <span class="badge badge-outline text-mini badge-xs w-16 h-5 text-gray-400">+{{ $countRole - 1 }} more</span>
-                                    @endif
-                                </div>
-                            @endif
+                        @foreach ($timeline['badgeRoles'] as $badgeRole)
+                            <div class="inline-flex gap-1">
+                                <span class="{{ $badgeRole['badgeRole'] }}">
+                                    {{ $badgeRole['role'] }}
+                                </span>
+                                @if ($badgeRole['moreCount'] > 0)
+                                    <span class="badge badge-outline text-mini badge-xs w-16 h-5 text-gray-400">+{{ $badgeRole['moreCount'] }} more</span>
+                                @endif
+                            </div>
                         @endforeach
                     </div>
                 </div>
                 <div class="flex flex-col gap-2 mt-2">
-                    <h5 class="text-sm">{{ Str::words($timeline->title, 3, '...') }}</h5>
-                    <span class="text-xs -mt-1 break-all">{{ $timeline->subtitle ?? '' }}</span>
+                    <h5 class="text-sm">{{ Str::words($timeline['timeline']->title, 3, '...') }}</h5>
+                    <span class="text-xs -mt-1 break-all">{{ $timeline['timeline']->subtitle ?? '' }}</span>
                 </div>
             </div>
         @endforeach
