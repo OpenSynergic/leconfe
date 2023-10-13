8ee9b86326a48e8713d7da03dca0d1ca3669049a
DikanR <dikan42724@gmail.com>
2023-10-13 12:00:05 +0800

    Update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        // $schedule->command('sitemap:generate')->daily();
     }
 
     /**
