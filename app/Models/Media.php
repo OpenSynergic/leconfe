8fc69b3ab640d0aa096c3fd8a9d87f901ad94dc2
Rahman Ramsi <rahmanramsi19@gmail.com>
2023-10-20 08:23:04 +0800

    restore media

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -3,22 +3,10 @@
 namespace App\Models;
 
 use GeneaLabs\LaravelModelCaching\Traits\Cachable;
-use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Kra8\Snowflake\HasShortflakePrimary;
-use Spatie\Image\Manipulations;
-use Spatie\MediaLibrary\HasMedia;
-use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media as Model;
 
-class Media extends Model implements HasMedia
+class Media extends Model
 {
-    use Cachable, HasShortflakePrimary, InteractsWithMedia;
-
-    public function file_name(Media $media = null): void
-    {
-        $this
-            ->addMediaConversion('preview')
-            ->fit(Manipulations::FIT_CROP, 300, 300)
-            ->nonQueued();
-    }
+    use Cachable, HasShortflakePrimary;
 }
