be1dc108689427ed17c7a404071711160419ebc3
Dede Nugroho <49790011+thisnugroho@users.noreply.github.com>
2023-11-01 11:36:56 +0800

    add: paper templates

--- app/Panel/Livewire/Workflows/PeerReviewSetting.php
+++ app/Panel/Livewire/Workflows/PeerReviewSetting.php
@@ -69,8 +69,27 @@ class PeerReviewSetting extends WorkflowStage implements HasForms, HasActions
                             ->label("Allowed File Types")
                             ->helperText("Allowed file types")
                             ->splitKeys([',', 'enter', ' ']),
+                        /**
+                         * Question:
+                         * 1. Should add min and max size?
+                         * 2. Should add max number of files?
+                         * 3. is the acceptedFileTypes is enough?
+                         */
                         SpatieMediaLibraryFileUpload::make('settings.paper_templates')
+                            ->model($this->conference)
+                            ->previewable(false)
+                            ->downloadable()
+                            ->disk('private-files')
+                            ->preserveFilenames()
+                            ->visibility('private')
+                            ->collection('paper-templates')
+                            ->acceptedFileTypes(
+                                ['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document']
+                            )
                             ->helperText("Upload paper templates")
+                            ->saveRelationshipsUsing(
+                                static fn (SpatieMediaLibraryFileUpload $component) => $component->saveUploadedFiles()
+                            )
                             ->label("Paper templates"),
                         TextInput::make('settings.invitation_response_days')
                             ->label("Invitation Response Deadline")
