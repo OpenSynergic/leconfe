ee6d62c6f5ac00cffc0692b88ecc60fe52c728bc
Dede Nugroho <49790011+thisnugroho@users.noreply.github.com>
2023-11-02 14:37:04 +0800

    add: mail notification

--- app/Panel/Livewire/Submissions/Forms/Publish.php
+++ app/Panel/Livewire/Submissions/Forms/Publish.php
@@ -3,8 +3,10 @@
 namespace App\Panel\Livewire\Submissions\Forms;
 
 use App\Actions\Submissions\SubmissionUpdateAction;
+use App\Mail\Templates\PublishSubmissionMail;
 use App\Models\Enums\SubmissionStage;
 use App\Models\Enums\SubmissionStatus;
+use App\Models\MailTemplate;
 use App\Models\Submission;
 use App\Panel\Livewire\Workflows\Classes\StageManager;
 use App\Panel\Livewire\Workflows\Concerns\InteractWithTenant;
@@ -14,12 +16,15 @@ use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Actions\Contracts\HasActions;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Form;
 use Filament\Forms\Get;
 use Filament\Infolists\Concerns\InteractsWithInfolists;
 use Filament\Infolists\Contracts\HasInfolists;
 use Filament\Infolists\Infolist;
+use Illuminate\Support\Facades\Mail;
 use Mohamedsabil83\FilamentFormsTinyeditor\Components\TinyEditor;
 
 class Publish extends \Livewire\Component implements HasActions, HasForms, HasInfolists
@@ -35,6 +40,9 @@ class Publish extends \Livewire\Component implements HasActions, HasForms, HasIn
             'status' => SubmissionStatus::Published
         ], $this->submission);
 
+        Mail::to($this->submission->user->email)
+            ->send(new PublishSubmissionMail($this->submission));
+
         $action->success();
     }
 
@@ -46,19 +54,31 @@ class Publish extends \Livewire\Component implements HasActions, HasForms, HasIn
             )
             ->authorize("Submission:publish")
             ->icon("iconpark-check")
-            ->label("Publish")
+            ->label("Send to Proceeding")
             ->successNotificationTitle("Submission published successfully")
+            ->mountUsing(function (Form $form) {
+                $mailTemplate = MailTemplate::where('mailable', PublishSubmissionMail::class)->first();
+                $form->fill([
+                    'email' => $this->submission->user->email,
+                    'subject' => $mailTemplate ? $mailTemplate->subject : '',
+                    'message' => $mailTemplate ? $mailTemplate->html_template : ''
+                ]);
+            })
             ->form([
                 Fieldset::make('Notification')
+                    ->columns(1)
                     ->schema([
-                        Checkbox::make('do-not-notify-author')
-                            ->reactive()
-                            ->label("Don't Send Notification to Author")
-                            ->columnSpanFull(),
+                        TextInput::make('email')
+                            ->disabled()
+                            ->dehydrated(),
+                        TextInput::make('subject')
+                            ->required(),
                         TinyEditor::make('message')
                             ->minHeight(300)
-                            ->hidden(fn (Get $get) => $get('do-not-notify-author'))
-                            ->columnSpanFull(),
+                            ->hidden(fn (Get $get) => $get('do-not-notify-author')),
+                        Checkbox::make('do-not-notify-author')
+                            ->reactive()
+                            ->label("Don't Send Notification to Author"),
                     ])
             ])
             ->action(fn (Action $action) => $this->handlePublishAction($action));
