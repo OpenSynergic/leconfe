9deb6995bce5dd85ed3125ca62de17bf5f2fa470
khfiii <khfii635@gmail.com>
2023-11-07 23:24:57 +0800

    Reset the spacing between  about page element

--- resources/views/conference/pages/about.blade.php
+++ resources/views/conference/pages/about.blade.php
@@ -1,8 +1,8 @@
 <x-website::layouts.main>
     <div class="card-body">
         <x-website::breadcrumbs :breadcrumbs="$this->getBreadcrumbs()" />
-            <div class="flex flex-col -space-y-3 mb-6 ms-1">
-            <h2 class="text-heading mt-6">{{ $this->getTitle() }} </h2>
+            <div class="flex flex-col -space-y-3 mb-6 mt-6 ms-1">
+            <h2 class="text-heading">{{ $this->getTitle() }} </h2>
             <div class="user-content">
                 {{ new Illuminate\Support\HtmlString($currentConference->getMeta('about')) }}
             </div>
