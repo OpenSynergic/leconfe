035ce953ac9153f6b85d3b7a272da8aea2bee520
Dikan Rizaldy <dikan42724@gmail.com>
2023-11-14 10:38:49 +0800

    Added AppearanceUpdateAction class

--- app/Actions/Conferences/ConferenceUpdateAction.php
+++ app/Actions/Conferences/ConferenceUpdateAction.php
@@ -18,24 +18,6 @@ class ConferenceUpdateAction
 
             $conference->update($data);
 
-            if (data_get($data, 'font')) {
-                $style = File::get(base_path('resources/website/css/website.css'));
-
-                $pattern = "/--font-family\s*:\s*'([\s*a-zA-Z0-9]+)'/";
-                $output = preg_replace($pattern, "--font-family: '{$data['font']}'", $style);
-
-                File::put(base_path('resources/website/css/website.css'), $output);
-            }
-
-            if (data_get($data, 'accent_color')) {
-                $config = File::get(base_path('resources/website/css/tailwind.config.js'));
-
-                $pattern = "/primary\s*:\s*'([#a-zA-Z0-9]+)'/";
-                $output = preg_replace($pattern, "primary: '{$data['accent_color']}'", $config);
-
-                File::put(base_path('resources/website/css/tailwind.config.js'), $output);
-            }
-
             if (data_get($data, 'meta')) {
                 $conference->setManyMeta(data_get($data, 'meta'));
             }
@@ -43,6 +25,8 @@ class ConferenceUpdateAction
             if (data_get($data, 'active')) {
                 ConferenceSetActiveAction::run($conference);
             }
+
+            AppearanceUpdateAction::run($data);
 
             DB::commit();
         } catch (\Throwable $th) {
