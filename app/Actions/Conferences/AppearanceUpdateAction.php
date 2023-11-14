a61f277b54dbc7d4e4fddf40e643689cf2211aac
Dikan Rizaldy <dikan42724@gmail.com>
2023-11-14 17:10:38 +0800

    Appearance settings in production WIP

--- app/Actions/Conferences/AppearanceUpdateAction.php
+++ app/Actions/Conferences/AppearanceUpdateAction.php
@@ -11,22 +11,30 @@ class AppearanceUpdateAction
 
     public function handle(array $data)
     {
-        if (data_get($data, 'font')) {
-            $style = File::get(base_path('resources/website/css/website.css'));
+        $changesCount = 0;
 
-            $pattern = "/--font-family\s*:\s*'([\s*a-zA-Z0-9]+)'/";
+        $style = File::get(base_path('resources/website/css/website.css'));
+        $pattern = "/--font-family\s*:\s*'([\s*a-zA-Z0-9]+)'/";
+        preg_match($pattern, $style, $matches);
+        if ($matches[1] != data_get($data, 'font')) {
             $output = preg_replace($pattern, "--font-family: '{$data['font']}'", $style);
-
             File::put(base_path('resources/website/css/website.css'), $output);
+
+            ++$changesCount;
         }
 
-        if (data_get($data, 'accent_color')) {
-            $config = File::get(base_path('resources/website/css/tailwind.config.js'));
-
-            $pattern = "/primary\s*:\s*'([#a-zA-Z0-9]+)'/";
+        $config = File::get(base_path('resources/website/css/tailwind.config.js'));
+        $pattern = "/primary\s*:\s*'([#a-zA-Z0-9]+)'/";
+        preg_match($pattern, $config, $matches);
+        if ($matches[1] != data_get($data, 'accent_color')) {
             $output = preg_replace($pattern, "primary: '{$data['accent_color']}'", $config);
-
             File::put(base_path('resources/website/css/tailwind.config.js'), $output);
+
+            ++$changesCount;
+        }
+
+        if (app()->isProduction() && $changesCount > 0) {
+            // Then run npm run build?
         }
     }
 }
