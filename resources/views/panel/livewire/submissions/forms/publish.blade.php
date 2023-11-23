65fc93b066312208b5484da0645d6d36b8fe4077
Dede Nugroho <49790011+thisnugroho@users.noreply.github.com>
2023-11-23 15:12:30 +0800

    Add conditional check for submission stage before publishing

--- resources/views/panel/livewire/submissions/forms/publish.blade.php
+++ resources/views/panel/livewire/submissions/forms/publish.blade.php
@@ -1,7 +1,9 @@
 <x-filament::section heading="Confiration Publishing">
     <div class="space-y-4">
         {{ $this->infolist }}
-        {{ $this->publishAction() }}
+        @if($submission->stage == App\Models\Enums\SubmissionStage::Editing)
+            {{ $this->publishAction() }}
+        @endif
     </div>
     <x-filament-actions::modals />
 </x-filament::section>
