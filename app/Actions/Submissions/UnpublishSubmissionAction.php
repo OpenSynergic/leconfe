0a0b7d3f6f7c0914d20b792f3d25d577e96834ab
Dede Nugroho <49790011+thisnugroho@users.noreply.github.com>
2023-12-07 09:36:12 +0800

    Fix: forgot to change the stage too.

--- app/Actions/Submissions/UnpublishSubmissionAction.php
+++ app/Actions/Submissions/UnpublishSubmissionAction.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Submissions;
 
+use App\Models\Enums\SubmissionStage;
 use App\Models\Enums\SubmissionStatus;
 use App\Models\Submission;
 use Lorisleiva\Actions\Concerns\AsAction;
@@ -14,6 +15,7 @@ class UnpublishSubmissionAction
     public function handle(Submission $submission)
     {
         SubmissionUpdateAction::run([
+            'stage' => SubmissionStage::Editing,
             'status' => SubmissionStatus::Editing,
         ], $submission);
     }
