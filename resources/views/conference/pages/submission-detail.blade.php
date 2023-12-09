5f83165eda2adeed8e65e3cc0a2be8722040f946
Dede Nugroho <49790011+thisnugroho@users.noreply.github.com>
2023-12-09 17:01:02 +0800

    Ui Improvement.

--- resources/views/conference/pages/submission-detail.blade.php
+++ resources/views/conference/pages/submission-detail.blade.php
@@ -1,6 +1,6 @@
 @use('App\Constants\SubmissionFileCategory')
 <x-website::layouts.main >
-    <div class="p-5">
+    <div class="p-5" id="submission-detail">
         <h1 class="text-xl text-primary">
             {{ $submission->getMeta('title') }}
         </h1>
@@ -10,11 +10,11 @@
                 {{ __('Date Published') . ': ' . $submission->published_at->format(setting('format.date')) }}
             </span>
         </div>
-        <h2 class="text-lg text-primary">
+        <h2 class="text-lg text-primary font-medium pb-1 mb-3 border-b border-b-slate-200">
             {{ __("Contributors") }}
         </h2>
         {{-- Contributors --}}
-        <div class="mt-1 p-5 bg-slate-100 border border-slate-200 shadow-sm rounded-md grid grid-cols-2 gap-4 text-slate-700 text-sm">
+        <div class="mt-3 p-5 bg-slate-100 border border-slate-200 shadow-sm rounded-md grid grid-cols-2 gap-4 text-slate-700 text-sm">
             @foreach($submission->contributors()->with(['participant', 'position'])->get() as $contributor)
                 <div class="col-span-1">
                     <div class="flex items-center">
@@ -27,15 +27,12 @@
         </div>
          {{-- Keywords --}}
          <div class="text-slate-800 mt-4">
-            <h2 class="text-lg text-primary">
+            <h2 class="text-lg text-primary font-medium pb-1 mb-3 border-b border-b-slate-200">
                 {{ __("Keywords") }}
             </h2>
-            <div class="flex flex-wrap text-xs space-x-2">
+            <div class="flex flex-wrap text-xs gap-3">
                 @foreach($submission->tagsWithType('submissionKeywords')->pluck('name') as $keyword)
-                    <a href="#" class="bg-slate-100 border border-slate-200 py-1 px-3 rounded-md link-primary flex items-center shadow-sm hover:bg-slate-200 hover:border-slate-300 transition ease-in-out duration-200">
-                        {{ $keyword }}
-                    </a>
-                    <a href="#" class="bg-slate-100 border border-slate-200 py-1 px-3 rounded-md link-primary flex items-center shadow-sm hover:bg-slate-200 hover:border-slate-300 transition ease-in-out duration-200">
+                    <a href="#" class="bg-slate-100 border border-slate-200 py-1 px-2 rounded-md link-primary flex items-center shadow-sm hover:bg-slate-200 hover:border-slate-300 transition ease-in-out duration-200">
                         {{ $keyword }}
                     </a>
                 @endforeach
@@ -43,14 +40,14 @@
         </div>
         {{-- Abstract --}}
         <div class="text-slate-800 mt-4">
-            <h2 class="text-lg text-primary">
+            <h2 class="text-lg text-primary font-medium pb-1 mb-3 border-b border-b-slate-200">
                 {{ __("Abstract") }}
             </h2>
             {!! $submission->getMeta('abstract') !!}
         </div>
         {{-- References --}}
-        <div class="text-slate-800 mt-4">
-            <h2 class="text-lg text-primary">
+        <div class="text-slate-800 mt-4" id="references">
+            <h2 class="text-lg text-primary font-medium pb-1 mb-3 border-b border-b-slate-200">
                 {{ __("References") }}
             </h2>
             @if($references = $submission->getMeta('references'))
