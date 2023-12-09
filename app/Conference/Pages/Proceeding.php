74ae9340e1437cd5caeb6486e64dd55028154ad7
Dede Nugroho <49790011+thisnugroho@users.noreply.github.com>
2023-12-09 12:01:10 +0800

    Refactor Proceeding class and add dynamic routing

--- app/Conference/Pages/Proceeding.php
+++ app/Conference/Pages/Proceeding.php
@@ -3,20 +3,32 @@
 namespace App\Conference\Pages;
 
 use App\Models\Topic;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Route;
+use Rahmanramsi\LivewirePageGroup\PageGroup;
 use Rahmanramsi\LivewirePageGroup\Pages\Page;
 
 class Proceeding extends Page
 {
     protected static string $view = 'conference.pages.proceeding';
 
-    public function mount()
+    public Collection $topics;
+
+    public function mount(?string $topicSlug = null)
     {
+        $this->topics = filled($topicSlug)
+            ? Topic::whereSlug($topicSlug)->get()
+            : Topic::get();
+
+        abort_if($this->topics->isEmpty(), 404);
     }
 
-    public function getViewData(): array
+    public static function routes(PageGroup $pageGroup): void
     {
-        return [
-            'topics' => Topic::get()
-        ];
+        $slug = static::getSlug();
+        Route::get("/{$slug}/{topicSlug?}", static::class)
+            ->middleware(static::getRouteMiddleware($pageGroup))
+            ->withoutMiddleware(static::getWithoutRouteMiddleware($pageGroup))
+            ->name((string) str($slug)->replace('/', '.'));
     }
 }
