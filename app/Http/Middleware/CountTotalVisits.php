030da92ec2010628bbc7b35fcd440d0afdfc5191
rayyan <khfii635@gmail.com>
2023-12-12 16:02:24 +0800

    Improve code in count total visit middleware

--- app/Http/Middleware/CountTotalVisits.php
+++ app/Http/Middleware/CountTotalVisits.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
 use App\Models\Conference;
-use Illuminate\Http\Request;
 use App\Website\Pages\Installation;
+use Closure;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Scopes\ConferenceScope;
 use Symfony\Component\HttpFoundation\Response;
 
 class CountTotalVisits
@@ -20,21 +19,42 @@ class CountTotalVisits
      */
     public function handle(Request $request, Closure $next): Response
     {
-
-        if (Route::getCurrentRoute()->uri === Installation::getSlug()) {
+        if ($this->isInstallationRoute()) {
             return $next($request);
         }
 
         $currentConference = Conference::active();
 
-        $currentConference->visit()
+        $this->recordVisit($request, $currentConference);
+
+        return $next($request);
+    }
+
+    /**
+     * Check if the current route is the installation route.
+     *
+     * @return bool
+     */
+    private function isInstallationRoute(): bool
+    {
+        return Route::getCurrentRoute()->uri === Installation::getSlug();
+    }
+
+    /**
+     * Record the visit for the current conference.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Conference  $conference
+     * @return void
+     */
+    private function recordVisit(Request $request, Conference $conference): void
+    {
+        $conference->visit()
             ->withIp()
             ->withSession()
             ->withUser()
             ->withData([
                 'user_agent' => $request->userAgent(),
             ]);
-
-        return $next($request);
     }
 }
