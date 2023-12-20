1a0db026257b4472196ac58d474a6620c4073a82
Dede Nugroho <49790011+thisnugroho@users.noreply.github.com>
2023-12-20 08:41:18 +0800

    style improvement

--- resources/views/conference/pages/proceeding.blade.php
+++ resources/views/conference/pages/proceeding.blade.php
@@ -1,44 +1,43 @@
 @use('App\Models\Enums\SubmissionStatus')
 @use('App\Constants\SubmissionFileCategory')
 <x-website::layouts.main>
-    <div class="p-5">
-        <div class="space-y-3">
-            @foreach ($topics as $topic)
-                <div class="font-semibold text-primary text-lg">
-                    {{ $topic->name }}
-                </div>
-                @forelse($submissions = $topic->submissions()->published()->get() as $submission)
-                    <div class="border-b border-b-slate-100 pb-4">
-                        <a href="{{ route('livewirePageGroup.current-conference.pages.submission-detail', $submission->getKey()) }}" class="link-primary text-base">
-                            {{ $submission->getMeta('title') }}
-                        </a>
+    <div class="submission-list p-5">
+        @foreach ($topics as $topic)
+            <div @class(['font-semibold text-2xl', 'mt-7' => !$loop->first])>
+                {{ $topic->name }}
+            </div>
+            @forelse($submissions = $topic->submissions()->published()->get() as $submission)
+                <div class="border-b border-b-slate-100 pb-4 mt-2">
+                    <a href="{{ route('livewirePageGroup.current-conference.pages.submission-detail', $submission->getKey()) }}"
+                        class="link-primary text-xl">
+                        {{ $submission->getMeta('title') }}
+                    </a>
 
-                        {{-- Contributors --}}
-                        <div class="text-xs flex items-center text-slate-700">
-                            <x-lineawesome-user class="w-3 h-3 mr-0.5"/>
-                            {{ $submission->contributors()->with('participant')->get()->pluck('participant.fullName')->join(', ') }}
-                        </div>
-                        <div class="text-xs flex items-center text-slate-700">
-                            <x-lineawesome-calendar-check-solid class="w-3 h-3 mr-0.5"/>
-                            {{ __('Date Published')  . ': ' . $submission->published_at->format(setting('format.date')) }}
-                        </div>
-                        @if(($files = $submission->getMedia(SubmissionFileCategory::EDITED_FILES)) && $files->count())
-                        <div class="flex flex-wrap gap-1.5 text-xs mt-2">
-                            @foreach($files as $file)
-                                <a href="{{ route('private.preview', $file->uuid ) }}" target="_blank" class="bg-slate-100 border border-slate-200 py-1 px-3 rounded-md link-primary flex items-center shadow-sm hover:bg-slate-200 hover:border-slate-300 transition ease-in-out duration-200">
+                    <div class="text-sm flex items-center text-slate-700">
+                        <x-lineawesome-user class="w-4 h-4 mr-0.5" />
+                        {{ $submission->contributors()->with('participant')->get()->pluck('participant.fullName')->join(', ') }}
+                    </div>
+                    <div class="text-sm flex items-center text-slate-700">
+                        <x-lineawesome-calendar-check-solid class="w-4 h-4 mr-0.5" />
+                        {{ __('Date Published') . ': ' . $submission->published_at->format(setting('format.date')) }}
+                    </div>
+                    @if (($files = $submission->getMedia(SubmissionFileCategory::EDITED_FILES)) && $files->count())
+                        <div class="flex flex-wrap gap-1.5 text-sm mt-2">
+                            @foreach ($files as $file)
+                                <a href="{{ route('private.preview', $file->uuid) }}" target="_blank"
+                                    class="break-all bg-slate-100 border border-slate-200 py-1 px-3 rounded-md link-primary flex items-center shadow-sm hover:bg-slate-200 hover:border-slate-300 transition ease-in-out duration-200">
                                     {{ $file->file_name }}
-                                    <x-lineawesome-external-link-square-alt-solid class="w-3 h-3 ml-0.5"/>
+                                    <x-lineawesome-external-link-square-alt-solid class="w-4 h-4 ml-0.5" />
                                 </a>
                             @endforeach
                         </div>
-                        @endif
-                    </div>
-                @empty
-                    <div class="text-xs text-gray-400">
-                        No Submission
-                    </div>
-                @endforelse
-            @endforeach
-        </div>
+                    @endif
+                </div>
+            @empty
+                <div class=" text-gray-400">
+                    No Submission
+                </div>
+            @endforelse
+        @endforeach
     </div>
 </x-website::layouts.main>
--- resources/views/conference/pages/submission-detail.blade.php
+++ resources/views/conference/pages/submission-detail.blade.php
@@ -1,76 +1,89 @@
 @use('App\Constants\SubmissionFileCategory')
-<x-website::layouts.main >
+<x-website::layouts.main>
     <div class="p-5" id="submission-detail">
-        <h1 class="text-xl text-primary">
+        <h1 class="text-2xl">
             {{ $submission->getMeta('title') }}
         </h1>
-        <div class="text-xs text-slate-400 mb-4">
+        <div class="text-sm text-slate-400 mb-4">
             <span class="flex items-center">
-                <x-lineawesome-calendar-check-solid class="w-3 h-3 mr-0.5"/>
+                <x-lineawesome-calendar-check-solid class="w-3 h-3 mr-0.5" />
                 {{ __('Date Published') . ': ' . $submission->published_at->format(setting('format.date')) }}
             </span>
         </div>
-        <h2 class="text-lg text-primary font-medium pb-1 mb-3 border-b border-b-slate-200">
-            {{ __("Contributors") }}
-        </h2>
-        {{-- Contributors --}}
-        <div class="mt-3 p-5 bg-slate-100 border border-slate-200 shadow-sm rounded-md grid grid-cols-2 gap-4 text-slate-700 text-sm">
-            @foreach($submission->contributors()->with(['participant', 'position'])->get() as $contributor)
-                <div class="col-span-1">
-                    <div class="flex items-center">
-                        <x-lineawesome-user class="w-4 h-4 mr-0.5"/>
-                        {{ $contributor->participant->fullName }}
-                    </div>
-                    <span class="ml-[17px] text-xs text-slate-500">{{ $contributor->position->name }}</span>
+        <div class="submission-detail space-y-7">
+            <section class="contributors">
+                <h2 class="text-xl font-medium pb-1 mb-3 border-b border-b-slate-200">
+                    {{ __('Contributors') }}
+                </h2>
+                {{-- Contributors --}}
+                <div
+                    class="mt-3 p-5 bg-slate-100 border border-slate-200 shadow-sm rounded-md grid grid-cols-2 gap-4 text-slate-700">
+                    @foreach ($submission->contributors()->with(['participant', 'position'])->get() as $contributor)
+                        <div class="col-span-1">
+                            <div class="flex items-center">
+                                <x-lineawesome-user class="w-5 h-5 mr-1" />
+                                {{ $contributor->participant->fullName }}
+                            </div>
+                            <span class="ml-[25px] text-sm text-slate-500">{{ $contributor->position->name }}</span>
+                        </div>
+                    @endforeach
                 </div>
-            @endforeach
-        </div>
-         {{-- Keywords --}}
-         <div class="text-slate-800 mt-4">
-            <h2 class="text-lg text-primary font-medium pb-1 mb-3 border-b border-b-slate-200">
-                {{ __("Keywords") }}
-            </h2>
-            <div class="flex flex-wrap text-xs gap-3">
-                @foreach($submission->tagsWithType('submissionKeywords')->pluck('name') as $keyword)
-                    <a href="#" class="bg-slate-100 border border-slate-200 py-1 px-2 rounded-md link-primary flex items-center shadow-sm hover:bg-slate-200 hover:border-slate-300 transition ease-in-out duration-200">
-                        {{ $keyword }}
-                    </a>
-                @endforeach
-            </div>
-        </div>
-        {{-- Abstract --}}
-        <div class="text-slate-800 mt-4">
-            <h2 class="text-lg text-primary font-medium pb-1 mb-3 border-b border-b-slate-200">
-                {{ __("Abstract") }}
-            </h2>
-            {!! $submission->getMeta('abstract') !!}
-        </div>
-        {{-- References --}}
-        <div class="text-slate-800 mt-4" id="references">
-            <h2 class="text-lg text-primary font-medium pb-1 mb-3 border-b border-b-slate-200">
-                {{ __("References") }}
-            </h2>
-            @if($references = $submission->getMeta('references'))
-                {!! $references !!}
-            @else
-                <span class="text-xs text-slate-400">
-                    {{ __("No References") }}
-                </span>
-            @endif
-        </div>
-        {{-- Downloads --}}
-        <div class="text-slate-800 mt-4">
-            <h2 class="text-lg text-primary">
-                {{ __("Downloads") }}
-            </h2>
-            <div class="flex flex-wrap gap-1.5 text-xs mt-2">
-                @foreach($submission->getMedia(SubmissionFileCategory::EDITED_FILES) as $file)
-                    <a href="{{ route('private.preview', $file->uuid) }}" target="_blank" class="bg-slate-100 border border-slate-200 py-1 px-3 rounded-md link-primary flex items-center shadow-sm hover:bg-slate-200 hover:border-slate-300 transition ease-in-out duration-200">
-                        {{ $file->file_name }}
-                        <x-lineawesome-external-link-square-alt-solid class="w-3 h-3 ml-0.5"/>
-                    </a>
-                @endforeach
-            </div>
+            </section>
+            {{-- Keywords --}}
+            <section class="keywords">
+                <div class="text-slate-800 mt-4">
+                    <h2 class="text-xl font-medium pb-1 mb-3 border-b border-b-slate-200">
+                        {{ __('Keywords') }}
+                    </h2>
+                    <div class="flex flex-wrap gap-3">
+                        @foreach ($submission->tagsWithType('submissionKeywords')->pluck('name') as $keyword)
+                            <span 
+                                class="bg-slate-100 border border-slate-200 py-1 px-2 rounded-md link-primary flex items-center shadow-sm hover:bg-slate-200 hover:border-slate-300 transition ease-in-out duration-200">
+                                {{ $keyword }}
+                            </span>
+                        @endforeach
+                    </div>
+                </div>
+            </section>
+            <section clas="abstract">
+                {{-- Abstract --}}
+                <div class="text-slate-800 mt-4">
+                    <h2 class="text-xl  font-medium pb-1 mb-3 border-b border-b-slate-200">
+                        {{ __('Abstract') }}
+                    </h2>
+                    {!! $submission->getMeta('abstract') !!}
+                </div>
+            </section>
+            <section class="references">
+                <div class="text-slate-800 mt-4" id="references">
+                    <h2 class="text-xl font-medium pb-1 mb-3 border-b border-b-slate-200">
+                        {{ __('References') }}
+                    </h2>
+                    @if ($references = $submission->getMeta('references'))
+                        {!! $references !!}
+                    @else
+                        <span class=" text-slate-400">
+                            {{ __('No References') }}
+                        </span>
+                    @endif
+                </div>
+            </section>
+            <section class="downloads">
+                <div class="text-slate-800 mt-4">
+                    <h2 class="text-xl">
+                        {{ __('Downloads') }}
+                    </h2>
+                    <div class="flex flex-wrap gap-1.5 mt-2">
+                        @foreach ($submission->getMedia(SubmissionFileCategory::EDITED_FILES) as $file)
+                            <a href="{{ route('private.preview', $file->uuid) }}" target="_blank"
+                                class="break-all bg-slate-100 border border-slate-200 py-1 px-3 rounded-md link-primary flex items-center shadow-sm hover:bg-slate-200 hover:border-slate-300 transition ease-in-out duration-200">
+                                {{ $file->file_name }}
+                                <x-lineawesome-external-link-square-alt-solid class="w-3 h-3 ml-0.5" />
+                            </a>
+                        @endforeach
+                    </div>
+                </div>
+            </section>
         </div>
     </div>
-    </x-website::layouts.main>
+</x-website::layouts.main>
