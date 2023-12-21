2c0db2b0fd48196cb27ea5f82c9e5b5cf9c59ff2
Rahman Ramsi <rahmanramsi19@gmail.com>
2023-12-21 09:11:06 +0800

    Add PaymentPolicy

---
+++ app/Policies/PaymentPolicy.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Payment;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class PaymentPolicy
+{
+     /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Payment $payment)
+    {
+        if($payment->isCompleted()){
+            return false;
+        }
+
+        if($user->can('Payment:update')){
+            return true;
+        }
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Payment $payment)
+    {
+        return false;
+    }
+}
