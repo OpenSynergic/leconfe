e15b5dbbf8ae702a93897923be5c6a8bd7785c3b
Rahman Ramsi <rahmanramsi19@gmail.com>
2023-12-30 08:17:03 +0800

    add error report using flareapp.io

---
+++ app/Providers/FlareServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use App\Application;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\ServiceProvider;
+use Spatie\LaravelIgnition\Facades\Flare;
+
+class FlareServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        Flare::determineVersionUsing(function() {
+            return Application::APP_VERSION;
+        });
+        Flare::group('Informations', [
+            'url' => url(),
+        ]);
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        if(!setting('send-error-report')){
+            Config::set('logging.channels.stack.channels', ['daily']);
+        }
+    }
+}
