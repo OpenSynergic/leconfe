8249b07dcca1286b917921a6be219a8fcd1336ee
Dede Nugroho <49790011+thisnugroho@users.noreply.github.com>
2024-01-08 19:36:16 +0800

    Hide button add message when topic closed

--- resources/views/panel/livewire/submissions/components/discussions/discussion-detail-form.blade.php
+++ resources/views/panel/livewire/submissions/components/discussions/discussion-detail-form.blade.php
@@ -1,10 +1,12 @@
 <div>
     <form wire:submit="submit" class="space-y-6">
         {{ $this->form }}
-        <div class="flex justify-end">
-            <x-filament::button type="submit" outlined="true" form="form" :disabled="!$topic->open">
-                {{ __('Add Message') }}
-            </x-filament::button>
-        </div>
+        @if($topic->open)
+            <div class="flex justify-end">
+                <x-filament::button type="submit" outlined="true" form="form">
+                    {{ __('Add Message') }}
+                </x-filament::button>
+            </div>
+        @endif
     </form>
 </div>
