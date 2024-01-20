a1751957cc22464dd5054fbd18bd7a82cb68f77a
Rahman Ramsi <rahmanramsi19@gmail.com>
2024-01-20 10:22:02 +0800

    add upgrade history log

---
+++ app/Actions/Leconfe/GetUpgradeActionHistory.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Actions\Leconfe;
+
+use Carbon\CarbonInterval;
+use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Collection;
+use Lorisleiva\Actions\Concerns\AsAction;
+use Lorisleiva\Actions\Concerns\AsCommand;
+use Spatie\Activitylog\Models\Activity;
+
+use function Laravel\Prompts\spin;
+
+class GetUpgradeActionHistory
+{
+    use AsAction;
+
+    public function handle() : Collection
+    {
+        return Activity::where('log_name', 'leconfe')
+            ->where('event', 'upgrade')
+            ->orderBy('created_at', 'desc')
+            ->get();
+    }
+
+    public function asCommand(Command $command): void
+    {
+        $upgradeHistories = spin(fn () => $this->handle(), 'Getting upgrade history');
+
+        if($upgradeHistories->isEmpty()) {
+            $command->info('No upgrade history found');
+            return;
+        }
+
+        $command->table(
+            ['From', 'To', 'Duration', 'Status', 'Run at'],
+            $upgradeHistories->map(function ($history) {
+                return [
+                    $history->getExtraProperty('from'),
+                    $history->getExtraProperty('to'),
+                    CarbonInterval::seconds($history->getExtraProperty('duration')),
+                    $history->getExtraProperty('status'),
+                    $history->created_at,
+                ];
+            })
+        );
+    }
+
+    public function getCommandSignature(): string
+    {
+        return 'leconfe:upgrade-history';
+    }
+
+    public function getCommandDescription(): string
+    {
+        return 'Check leconfe upgrade history';
+    }
+}
