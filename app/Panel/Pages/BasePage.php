1ffc51dab7b100d2307df7e4e5cefad1ce82ab43
Dede Nugroho <49790011+thisnugroho@users.noreply.github.com>
2024-03-16 09:38:28 +0800

    Refactor BasePage to be abstract

--- app/Panel/Pages/BasePage.php
+++ app/Panel/Pages/BasePage.php
@@ -5,7 +5,7 @@ namespace App\Panel\Pages;
 use App\Panel\Pages\Traits\CustomizedUrl;
 use Filament\Pages\Page;
 
-class BasePage extends Page
+abstract class BasePage extends Page
 {
     use CustomizedUrl;
 }
