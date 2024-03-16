6cfdad7b07439201089a1e506d2bb770582b8474
Dede Nugroho <49790011+thisnugroho@users.noreply.github.com>
2024-03-16 09:09:20 +0800

    fix: can't access static pages

--- app/Panel/Resources/StaticPageResource.php
+++ app/Panel/Resources/StaticPageResource.php
@@ -3,11 +3,8 @@
 namespace App\Panel\Resources;
 
 use App\Administration\Resources\StaticPageResource as StaticPageResourceBase;
-use App\Panel\Resources\Traits\CustomizedUrl;
 
 class StaticPageResource extends StaticPageResourceBase
 {
     protected static ?string $navigationGroup = 'Conferences';
-
-    use CustomizedUrl;
 }
