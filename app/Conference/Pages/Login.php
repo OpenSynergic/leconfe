42202a0ec1e9540908d17d566efa1e9ce2b6eb8b
Dede Nugroho <49790011+thisnugroho@users.noreply.github.com>
2024-03-16 08:02:57 +0800

    update conference login

--- app/Conference/Pages/Login.php
+++ app/Conference/Pages/Login.php
@@ -2,8 +2,69 @@
 
 namespace App\Conference\Pages;
 
-use App\Website\Pages\Login as PagesLogin;
+use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
+use DanHarrin\LivewireRateLimiting\WithRateLimiting;
+use Filament\Facades\Filament;
+use Illuminate\Validation\ValidationException;
+use Livewire\Attributes\Rule;
+use Rahmanramsi\LivewirePageGroup\Pages\Page;
 
-class Login extends PagesLogin
+class Login extends Page
 {
+    use WithRateLimiting;
+
+    protected static string $view = 'conference.pages.login';
+
+    #[Rule('required|email')]
+    public ?string $email = null;
+
+    #[Rule('required')]
+    public ?string $password = null;
+
+    #[Rule('boolean')]
+    public bool $remember = false;
+
+    public function mount()
+    {
+        if (Filament::auth()->check()) {
+            $this->redirect(Filament::getUrl(), navigate: false);
+        }
+    }
+
+    public function getBreadcrumbs(): array
+    {
+        return [
+            url('/') => 'Home',
+            'Login',
+        ];
+    }
+
+    public function login()
+    {
+        try {
+            $this->rateLimit(5);
+        } catch (TooManyRequestsException $exception) {
+            $this->addError('email', __('auth.throttle', [
+                'seconds' => $exception->secondsUntilAvailable,
+                'minutes' => ceil($exception->secondsUntilAvailable / 60),
+            ]));
+
+            return null;
+        }
+
+        $this->validate();
+
+        if (!Filament::auth()->attempt([
+            'email' => $this->email,
+            'password' => $this->password,
+        ], $this->remember)) {
+            throw ValidationException::withMessages([
+                'email' => __('auth.failed'),
+            ]);
+        }
+
+        session()->regenerate();
+
+        $this->redirect(Filament::getUrl(), navigate: false);
+    }
 }
