68c5d4a4a594f643d6d009b36a4f73724a450978
Dede Nugroho <49790011+thisnugroho@users.noreply.github.com>
2024-03-16 09:29:51 +0800

    Add BasePage and BaseResource classes with customized URL trait

--- app/Panel/Resources/BaseResource.php
+++ app/Panel/Resources/BaseResource.php
@@ -2,20 +2,10 @@
 
 namespace App\Panel\Resources;
 
-use Filament\Facades\Filament;
-use Illuminate\Database\Eloquent\Model;
+use App\Panel\Resources\Traits\CustomizedUrl;
+use Filament\Resources\Resource;
 
-abstract class BaseResource extends \Filament\Resources\Resource
+abstract class BaseResource extends Resource
 {
-    public static function getUrl(string $name = 'index', array $parameters = [], bool $isAbsolute = true, ?string $panel = null, ?Model $tenant = null): string
-    {
-        if (blank($panel) || Filament::getPanel($panel)->hasTenancy()) {
-            $parameters['tenant'] ??= ($tenant ?? Filament::getTenant());
-        }
-
-        $routeBaseName = static::getRouteBaseName(panel: $panel);
-        $parameters['conference'] ??= app()->getCurrentConference()->path;
-
-        return route("{$routeBaseName}.{$name}", $parameters, $isAbsolute);
-    }
+    use CustomizedUrl;
 }
