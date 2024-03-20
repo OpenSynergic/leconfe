96907afcdf69066ad866af1b4c4e315160f69a2c
faisalwahyudi10 <faisal.piffent.s@gmail.com>
2024-03-20 16:07:18 +0800

    create-speakers-migration-and-model

---
+++ database/migrations/2024_03_20_152327_create_speakers_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use App\Models\Conference;
+use App\Models\SpeakerRole;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('speaker_roles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Conference::class)->constrained();
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->string('type');
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->unsignedInteger('order_column')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('speaker_meta', function (Blueprint $table) {
+            $table->id();
+            $table->string('metable_type');
+            $table->unsignedBigInteger('metable_id');
+            $table->string('type')->default('null');
+            $table->string('key')->index();
+            $table->longtext('value');
+
+            $table->unique(['metable_type', 'metable_id', 'key']);
+            $table->index(['key', 'metable_type']);
+        });
+
+        Schema::create('speakers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(SpeakerRole::class)->constrained();
+            $table->string('email')->nullable()->unique();
+            $table->string('given_name');
+            $table->string('family_name')->nullable();
+            $table->string('public_name')->nullable();
+            $table->unsignedInteger('order_column')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('speakers');
+    }
+};
