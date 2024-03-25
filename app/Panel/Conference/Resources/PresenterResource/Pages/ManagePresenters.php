9281904a4d4fc949d01b129548a02a30b16db14c
faisalwahyudi10 <faisal.piffent.s@gmail.com>
2024-03-25 11:31:58 +0800

    change-to-manage-record

--- app/Panel/Conference/Resources/PresenterResource/Pages/ManagePresenters.php
+++ app/Panel/Conference/Resources/PresenterResource/Pages/ManagePresenters.php
@@ -4,9 +4,10 @@ namespace App\Panel\Conference\Resources\PresenterResource\Pages;
 
 use App\Panel\Conference\Resources\PresenterResource;
 use Filament\Actions;
-use Filament\Resources\Pages\CreateRecord;
+use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Pages\ManageRecords;
 
-class CreatePresenter extends CreateRecord
+class ManagePresenters extends ManageRecords
 {
     protected static string $resource = PresenterResource::class;
 }
