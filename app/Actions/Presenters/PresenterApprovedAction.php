e2c625606242ec7e11d106038f42414b05dbaa6e
faisalwahyudi10 <faisal.piffent.s@gmail.com>
2024-03-26 16:11:11 +0800

    add-presenter-action

---
+++ app/Actions/Presenters/PresenterApprovedAction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Actions\Presenters;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class PresenterApprovedAction
+{
+    use AsAction;
+
+    public function handle()
+    {
+        // ...
+    }
+}
