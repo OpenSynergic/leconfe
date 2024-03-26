1ddd9a7cd9a71cd9e201f01ba81012a7d6f1a11f
faisalwahyudi10 <faisal.piffent.s@gmail.com>
2024-03-26 16:11:37 +0800

    create-mail-template-reject-presenter

---
+++ app/Mail/Templates/RejectedPresenterMail.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Mail\Templates;
+
+use App\Classes\Log;
+use App\Mail\Templates\Traits\CanCustomizeTemplate;
+use App\Models\Presenter;
+use App\Models\Submission;
+
+class RejectedPresenterMail extends TemplateMailable
+{
+    use CanCustomizeTemplate;
+
+    public string $presenter;
+    
+    public string $title;
+
+    public string $loginLink;
+
+    public ?string $message;
+
+    public Log $log;
+
+    public function __construct(protected Presenter $presenterModel)
+    {
+        $this->presenter = $presenterModel->fullName;
+        $this->title = $presenterModel->submission->getMeta('title');
+        $this->loginLink = route('livewirePageGroup.website.pages.login');
+
+        $this->log = Log::make(
+            name: 'email',
+            subject: $presenterModel->submission,
+            description: __('log.email.sent', ['name' => 'Rejected Presenter']),
+        );
+
+        $this->message = (string) $this->buildView()['text'];
+    }
+
+    public static function getDefaultSubject(): string
+    {
+        return 'You have been rejected as a presenter';
+    }
+
+    public static function getDefaultDescription(): string
+    {
+        return 'This email is sent to presenters when they are rejected from a submission';
+    }
+
+    public static function getDefaultHtmlTemplate(): string
+    {
+        return <<<'HTML'
+            <p>This is a automatic notification to let you know that {{ presenter }} has been rejected from the submission titled "{{ title }}".</p>
+        HTML;
+    }
+}
