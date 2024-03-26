11a4874a87800e5454d230e34436fe8e3a171d11
faisalwahyudi10 <faisal.piffent.s@gmail.com>
2024-03-26 16:12:29 +0800

    presenter-resource-WIP

--- app/Panel/Conference/Resources/PresenterResource.php
+++ app/Panel/Conference/Resources/PresenterResource.php
@@ -3,25 +3,31 @@
 namespace App\Panel\Conference\Resources;
 
 use Filament\Tables;
+use App\Models\Timeline;
 use Filament\Forms\Form;
 use App\Models\Presenter;
 use Filament\Tables\Table;
 use Squire\Models\Country;
+use App\Models\MailTemplate;
 use Filament\Resources\Resource;
 use Filament\Tables\Grouping\Group;
+use Illuminate\Support\Facades\Mail;
 use App\Models\Enums\PresenterStatus;
-use App\Models\Timeline;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\Textarea;
 use Filament\Tables\Columns\TextColumn;
 use Illuminate\Database\Eloquent\Model;
+use Filament\Tables\Columns\Layout\Panel;
 use Filament\Tables\Columns\Layout\Split;
 use Filament\Tables\Columns\Layout\Stack;
 use Illuminate\Database\Eloquent\Builder;
+use App\Mail\Templates\RejectedPresenterMail;
+use App\Actions\Presenters\PresenterRejectedAction;
 use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
 use App\Panel\Conference\Resources\PresenterResource\Pages;
-use Filament\Forms\Components\Select;
-use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\HtmlString;
+use Mohamedsabil83\FilamentFormsTinyeditor\Components\TinyEditor;
 
 class PresenterResource extends Resource
 {
@@ -49,7 +55,7 @@ class PresenterResource extends Resource
     {
         return $table
             ->defaultGroup('submission.id')
-            ->groupingSettingsHidden()
+            // ->groupingSettingsHidden()
             ->groups([
                 Group::make('submission.id')
                     ->label('Group by Submission')
@@ -92,124 +98,156 @@ class PresenterResource extends Resource
                                     ->orWhere('given_name', 'LIKE', "%{$search}%")
                                     ->orWhere('family_name', 'LIKE', "%{$search}%")
                             ),
-                        TextColumn::make('affiliation')
-                            ->size('xs')
-                            ->getStateUsing(
-                                fn (Model $record) => $record->getMeta('affiliation')
-                            )
-                            ->icon('heroicon-o-building-library')
-                            ->searchable(
-                                query: fn ($query, $search) => $query
-                                    ->whereMeta('affiliation', 'LIKE', "%{$search}%")
-                            )
-                            ->extraAttributes([
-                                'class' => 'text-xs',
-                            ])
-                            ->color('gray'),
-                        TextColumn::make('email')
-                            ->size('xs')
-                            ->extraAttributes([
-                                'class' => 'text-xs',
-                            ])
-                            ->searchable()
-                            ->color('gray')
-                            ->icon('heroicon-o-envelope')
-                            ->alignStart(),
+                        Split::make([
+                            TextColumn::make('affiliation')
+                                ->size('xs')
+                                ->getStateUsing(
+                                    fn (Model $record) => $record->getMeta('affiliation')
+                                )
+                                ->icon('heroicon-o-building-library')
+                                ->searchable(
+                                    query: fn ($query, $search) => $query
+                                        ->whereMeta('affiliation', 'LIKE', "%{$search}%")
+                                )
+                                ->extraAttributes([
+                                    'class' => 'text-xs',
+                                ])
+                                ->color('gray')
+                                ,
+                            TextColumn::make('email')
+                                ->size('xs')
+                                ->extraAttributes([
+                                    'class' => 'text-xs',
+                                ])
+                                ->searchable()
+                                ->color('gray')
+                                ->icon('heroicon-o-envelope')
+                                ->alignStart(),
+                        ])
+                        ->extraAttributes([
+                            'class' => 'w-fit',
+                        ]),
+                        Panel::make([
+                            TextColumn::make('notes')
+                                ->label('Notes')
+                                ->size('xs')
+                                ->getStateUsing(function (Presenter $record) {
+                                    return new HtmlString(<<<HTML
+                                        <div class="space-y-1 text-xs">
+                                            <p class="text-gray-500">Notes :</p>
+                                            <p class="text-gray-700">{$record->getMeta('notes')}</p>
+                                        </div>
+                                    HTML);
+                                })
+                        ])
+                        ->visible(fn (Model $record) => $record->getMeta('notes'))
+                        ->collapsible()
+                        ->extraAttributes([
+                            'class' => 'w-1/2',
+                        ]),
                     ])
                     ->space(1),
-                    TextColumn::make('status')
-                        ->label('Status')
-                        ->badge()
-                        ->color(function (Model $record) {
-                            return $record->status->getColor();
-                        })
-                        ->searchable()
-                        ->alignCenter(),
+                    // TextColumn::make('status')
+                    //     ->label('Status')
+                    //     ->badge()
+                    //     ->color(function (Model $record) {
+                    //         return $record->status->getColor();
+                    //     })
+                    //     ->searchable()
+                    //     ->alignCenter(),
                 ]),
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\Action::make('approve')
-                    ->label('Approve')
-                    ->modalHeading('Approve Presenter')
-                    ->icon('heroicon-o-check')
-                    ->iconSize('md')
-                    ->color('primary')
-                    ->form([
-                        Select::make('timeline')
-                            ->label('Timeline')
-                            ->required()
-                            ->searchable()
-                            ->options(function () {
-                                return Timeline::query()
-                                    ->get()
-                                    ->pluck('title', 'id')
-                                    ->toArray();
-                            }),
-                        Textarea::make('note')
-                            ->label('Note')
-                            ->required()
-                            ->placeholder('Enter your note here...')
-                            ->helperText('This note will be sent to and seen by the presenter.')
-                            ->rows(3),
-                        Checkbox::make('do-not-notify-presenter')
-                            ->label("Don't Send Notification to Presenter"),
-                    ])
-                    ->action(fn (Presenter $presenter) => $presenter->update(['status' => PresenterStatus::Approve])),
-                Tables\Actions\Action::make('reject')
-                    ->label('Reject')
-                    ->modalHeading('Reject Presenter')
-                    ->icon('heroicon-o-x-mark')
-                    ->iconSize('md')
-                    ->color('danger')
-                    ->form([
-                        Textarea::make('recommendation')
-                            ->label('Recommendation')
-                            ->required()
-                            ->placeholder('Enter your recommendation here...')
-                            ->helperText('This note will be sent to and seen by the presenter.')
-                            ->rows(3),
-                        Checkbox::make('do-not-notify-presenter')
-                            ->label("Don't Send Notification to Presenter"),
-                    ])
-                    ->successNotificationTitle('The presenter has been rejected.')
-                    ->action(function (Tables\Actions\Action $action, array $data, Presenter $record) {
-                        $presenter = Presenter::find($action->getRecordId());
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\Action::make('approve')
+                        ->label('Approve')
+                        ->modalHeading('Approve Presenter')
+                        ->icon('heroicon-o-check')
+                        ->iconSize('md')
+                        ->color('primary')
+                        ->form([
+                            Select::make('timeline')
+                                ->label('Timeline')
+                                ->required()
+                                ->searchable()
+                                ->options(function () {
+                                    return Timeline::query()
+                                        ->get()
+                                        ->pluck('title', 'id')
+                                        ->toArray();
+                                }),
+                            Textarea::make('note')
+                                ->label('Note')
+                                ->required()
+                                ->placeholder('Enter your note here...')
+                                ->helperText('This note will be sent to and seen by the presenter.')
+                                ->rows(3),
+                            Checkbox::make('do-not-notify-presenter')
+                                ->label("Don't Send Notification to Presenter"),
+                        ])
+                        ->action(fn (Presenter $presenter) => $presenter->update(['status' => PresenterStatus::Approve])),
+                    Tables\Actions\Action::make('reject')
+                        ->label('Reject')
+                        ->modalHeading('Reject Presenter')
+                        ->icon('heroicon-o-x-mark')
+                        ->iconSize('md')
+                        ->color('danger')
+                        ->hidden(fn (Presenter $record) => $record->status == PresenterStatus::Reject)
+                        ->mountUsing(function (Form $form): void {
+                            $mailTemplate = MailTemplate::where('mailable', RejectedPresenterMail::class)->first();
+                            $form->fill([
+                                'notes' => $mailTemplate ? $mailTemplate->html_template : '',
+                            ]);
+                        })
+                        ->form([
+                            TinyEditor::make('notes')
+                                ->label('Notes')
+                                ->profile('email')
+                                ->required()
+                                ->placeholder('Enter your note here...')
+                                ->helperText('This note will be sent to and seen by the presenter.'),
+                            Checkbox::make('do-not-notify-presenter')
+                                ->label("Don't Send Notification to Presenter"),
+                        ])
+                        ->successNotificationTitle('The presenter has been rejected.')
+                        ->action(function (Tables\Actions\Action $action, array $data, Presenter $record) {
+                            $rejectedPresenter = PresenterRejectedAction::run($record);
+                            
+                            $mailTemplate = MailTemplate::where('mailable', RejectedPresenterMail::class)->first();
+                            $getTemplateMail = (new RejectedPresenterMail($rejectedPresenter))
+                                ->subjectUsing($mailTemplate->subject)
+                                ->contentUsing($data['notes']);
 
-                        $presenter->update([
-                            'status' => PresenterStatus::Reject,
-                        ]);
+                            $rejectedPresenter->setManyMeta([
+                                'notes' => $getTemplateMail->message,
+                                'rejected_by' => auth()->user()->id,
+                            ]);
 
-                        if (! $data['do-not-notify-presenter']) {
-                            try {
-                                // Mail::to($record->email)
-                                //     ->send(
-                                //         (new RevisionRequestMail($this->submission))
-                                //             ->subjectUsing($data['subject'])
-                                //             ->contentUsing($data['message'])
-                                //     );
-                            } catch (\Exception $e) {
-                                $action->failureNotificationTitle('The email notification was not delivered.');
-                                $action->failure();
+                            if (! $data['do-not-notify-presenter']) {
+                                try {
+                                    Mail::to($record->email)
+                                        ->send($getTemplateMail);
+                                } catch (\Exception $e) {
+                                    $action->failureNotificationTitle('The email notification was not delivered.');
+                                    $action->failure();
+                                }
                             }
-                        }
 
-                        $action->success();
-                    })
-                
+                            $action->success();
+                        })
+                ])
+                ->label(fn (Presenter $record) => $record->status == PresenterStatus::Unchecked ? 'Set Decision' : $record->status->getActionText())
+                ->icon(fn (Presenter $record) => $record->status->getActionIcon())
+                ->color(fn (Presenter $record) => $record->status->getActionColor())
+                ->button()
+                ->outlined()
             ])
             ->bulkActions([
                 //
             ]);
-    }
-
-    public static function getRelations(): array
-    {
-        return [
-            //
-        ];
     }
 
     public static function getPages(): array
--- app/Panel/Conference/Resources/PresenterResource/Pages/ManagePresenters.php
+++ app/Panel/Conference/Resources/PresenterResource/Pages/ManagePresenters.php
@@ -2,12 +2,34 @@
 
 namespace App\Panel\Conference\Resources\PresenterResource\Pages;
 
+use App\Models\Enums\PresenterStatus;
+use App\Models\Presenter;
 use App\Panel\Conference\Resources\PresenterResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Resources\Pages\ManageRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ManagePresenters extends ManageRecords
 {
     protected static string $resource = PresenterResource::class;
+
+    public function getTabs(): array
+    {
+        return [
+            'unchecked' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', PresenterStatus::Unchecked))
+                ->badge(Presenter::where('status', PresenterStatus::Unchecked)->count())
+                ->badgeColor(Presenter::where('status', PresenterStatus::Unchecked)->count() > 0 ? 'primary' : 'gray'),
+            'approved' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', PresenterStatus::Approve))
+                ->badge(Presenter::where('status', PresenterStatus::Approve)->count())
+                ->badgeColor(Presenter::where('status', PresenterStatus::Approve)->count() > 0 ? 'primary' : 'gray'),
+            'rejected' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', PresenterStatus::Reject))
+                ->badge(Presenter::where('status', PresenterStatus::Reject)->count())
+                ->badgeColor(Presenter::where('status', PresenterStatus::Reject)->count() > 0 ? 'primary' : 'gray'),
+        ];
+    }
 }
