9f87c5221dc8e765f91423e9466e64a37e50a760
Rahman Ramsi <rahmanramsi19@gmail.com>
2024-04-04 13:49:27 +0800

    fix footer examples get sanitized

--- resources/views/frontend/examples/footer.blade.php
+++ resources/views/frontend/examples/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-white rounded-lg shadow dark:bg-gray-800">
+<div class="bg-white rounded-lg shadow dark:bg-gray-800">
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
      <div class="inline-flex items-center gap-2">
         <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://leconfe.com/" class="link link-hover link-primary">Leconfe™</a>. All Rights Reserved.</span>
@@ -18,4 +18,4 @@
         </li>
     </ul> --}}
     </div>
-</footer>
+</div>
