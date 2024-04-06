69115367162418ec76d75bd64fc5ff29b65314da
Wahni Adnani <adnaniwahni@gmail.com>
2024-04-06 14:35:29 +0800

    fix venues migration

--- database/migrations/2024_04_06_101953_create_venues_table.php
+++ database/migrations/2024_04_06_101953_create_venues_table.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Models\Conference;
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\Serie;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('venues', function (Blueprint $table) {
             $table->id();
-            // $table->foreignIdFor(Conference::class)->constrained();
+            $table->foreignIdFor(Serie::class)->constrained();
             $table->string('name');
             $table->string('location');
             $table->text('description')->nullable();
