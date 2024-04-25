5adf177eabae19bbc87daa3df61519e31be1e197
2010010629 <abdulwahid120600@gmail.com>
2024-04-25 11:22:58 +0800

    move function for setting to classes settings

--- app/Panel/Conference/Pages/Dashboard.php
+++ app/Panel/Conference/Pages/Dashboard.php
@@ -7,4 +7,17 @@ use Filament\Pages\Dashboard as PagesDashboard;
 
 class Dashboard extends PagesDashboard
 {
+    // public function mount()
+    // {
+    //     $data = [
+    //         'test' => [
+    //             'chid1' => true,
+    //             'child2' => false
+    //         ]
+    //     ];
+    //     // $setting = Settings::all();
+    //     // dd($setting['mail']['auth'])
+    //     // dd(Settings::all());
+    //     dd(Settings::get('mail.from.address'));
+    // }
 }
