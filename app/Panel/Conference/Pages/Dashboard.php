7211a51cb49dfecc1bb022dedf52fc8805bee107
2010010629 <abdulwahid120600@gmail.com>
2024-04-26 10:53:21 +0800

    update

--- app/Panel/Conference/Pages/Dashboard.php
+++ app/Panel/Conference/Pages/Dashboard.php
@@ -2,15 +2,8 @@
 
 namespace App\Panel\Conference\Pages;
 
-use App\Facades\Settings;
 use Filament\Pages\Dashboard as PagesDashboard;
 
 class Dashboard extends PagesDashboard
 {
-    public function mount()
-    {
-        $settings = Settings::all();
-        // dd($settings);
-        // dd($settings['mail_auth_port'] ?? null);
-    }
 }
