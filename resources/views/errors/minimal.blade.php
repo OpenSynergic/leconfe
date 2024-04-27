26f7664ef9205332024535bd9094bbd133947da0
2010010629 <abdulwahid120600@gmail.com>
2024-04-27 11:40:12 +0800

    remove animation

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -1,16 +1,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    {{ MetaTag::render() }}
-
     <title>
         @yield('title')
     </title>
     <meta name="application-name" content="Leconfe">
-    <meta name=generator content="Leconfe {{ app()->getCodeVersion() }}">
-    @if(isset($favicon))
-        <link rel="icon" type="image/x-icon" href="{{ $favicon }}">
-    @endif
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
