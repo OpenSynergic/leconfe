1901eaff68a7b0a9875c52954b3a21b2eac1e835
2010010629 <abdulwahid120600@gmail.com>
2024-04-27 12:01:21 +0800

    delete error pages

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -1,29 +1,8 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>
-        @yield('title')
-    </title>
-    <meta name="application-name" content="Leconfe">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    @livewireStyles
-    @vite(['resources/frontend/css/frontend.css', 'resources/frontend/js/frontend.js'])
-</head>
-    <body>
-        <div class="flex flex-col h-screen gap-3 min-h-screen">
-            <div class="navbar-container sticky top-0 z-[60] bg-primary text-white shadow">
-                <div class="navbar mx-auto max-w-7xl">
-                    <div class="navbar-start items-center w-auto sm:w-1/2 gap-2">
-                        <x-website::navigation-menu-mobile />
-                        <x-website::logo/>
-                    </div>
-                </div>
-            </div>
-            <x-website::layouts.error></x-website::layouts.error>
-            <x-website::layouts.footer></x-website::layouts.footer>
-        </div>
-        @livewireScripts
-    </body>
-</html>
+<x-website::layouts.main>
+    <div class="text-center">
+        <h1 class="mb-4 text-6xl font-semibold text-red-500">
+            @yield('code')
+        </h1>
+        <p class="mb-4 text-lg text-gray-600">@yield('message')</p>
+    </div>
+</x-website::layouts.main>
--- resources/views/frontend/website/components/layouts/error.blade.php
+++
@@ -1,8 +0,0 @@
-<x-website::layouts.main>
-    <div class="text-center">
-        <h1 class="mb-4 text-6xl font-semibold text-red-500">
-            @yield('code')
-        </h1>
-        <p class="mb-4 text-lg text-gray-600">@yield('message')</p>
-    </div>
-</x-website::layouts.main>
