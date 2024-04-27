965b8b03044b106a6750a0e388f72e1e1d137ffb
2010010629 <abdulwahid120600@gmail.com>
2024-04-27 17:02:57 +0800

    change error page

---
+++ resources/views/frontend/website/pages/error.blade.php
@@ -0,0 +1,6 @@
+<x-website::layouts.main>
+    <div class="text-center">
+        <h1 class="mb-4 text-6xl font-semibold text-red-500">testttt</h1>
+        <p class="mb-4 text-lg text-gray-600">@yield('message')</p>
+    </div>
+</x-website::layouts.main>
