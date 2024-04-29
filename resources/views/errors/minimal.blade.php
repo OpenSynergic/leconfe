36da03688031fd47149aea34184d5c16399e6a05
2010010629 <abdulwahid120600@gmail.com>
2024-04-29 16:59:26 +0800

    make error page

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -1,1 +1,8 @@
-@extends('frontend.website.pages.error')
+<x-website::layouts.app>
+    <div class="text-center">
+        <h1 class="mb-4 text-6xl font-semibold text-red-500">
+            @yield('code')
+        </h1>
+        <p class="mb-4 text-lg text-gray-600">@yield('message')</p>
+    </div>
+</x-website::layouts.app>
--- resources/views/frontend/website/pages/error.blade.php
+++
@@ -1,6 +0,0 @@
-<x-website::layouts.main>
-    <div class="text-center">
-        <h1 class="mb-4 text-6xl font-semibold text-red-500">testttt</h1>
-        <p class="mb-4 text-lg text-gray-600">@yield('message')</p>
-    </div>
-</x-website::layouts.main>
