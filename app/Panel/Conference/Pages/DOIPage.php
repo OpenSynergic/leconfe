ae9183316b2151907f109b152cf1946c2c7b7fa0
Rahman Ramsi <rahmanramsi19@gmail.com>
2024-05-03 15:33:42 +0800

    configure access based on doi config

--- app/Panel/Conference/Pages/DOIPage.php
+++ app/Panel/Conference/Pages/DOIPage.php
@@ -17,11 +17,19 @@ class DOIPage extends Page
 
     public static function canAccess(): bool
     {
-        return app()->getCurrentConference()->getMeta('doi_enabled');
+        return app()->getCurrentConference()?->getMeta('doi_enabled') && !empty(app()->getCurrentConference()?->getMeta('doi_items', []));
     }
 
     public function mount()
     {
+    }
+
+    public function getViewData(): array
+    {
+        return [
+            'articlesDoiEnabled' => in_array('articles', app()->getCurrentConference()?->getMeta('doi_items', [])),
+            'proceedingsDoiEnabled' => in_array('proceedings', app()->getCurrentConference()?->getMeta('doi_items', [])),
+        ];
     }
 
     public static function getRoutePath(): string
--- resources/views/panel/conference/pages/doi-page.blade.php
+++ resources/views/panel/conference/pages/doi-page.blade.php
@@ -1,25 +1,34 @@
 <x-filament-panels::page x-data="{ activeTab: 'submissions' }">
     <x-filament::tabs>
-        <x-filament::tabs.item
-            alpine-active="activeTab === 'submissions'"
-            x-on:click="activeTab = 'submissions'"
-            >
-            Submissions
-        </x-filament::tabs.item>
+        @if($articlesDoiEnabled)
+            <x-filament::tabs.item
+                alpine-active="activeTab === 'submissions'"
+                x-on:click="activeTab = 'submissions'"
+                >
+                Submissions
+            </x-filament::tabs.item>
+        @endif
 
-        <x-filament::tabs.item
+        @if ($proceedingsDoiEnabled)
+            <x-filament::tabs.item
             alpine-active="activeTab === 'proceedings'"
             x-on:click="activeTab = 'proceedings'"
-        >
-            Proceedings
-        </x-filament::tabs.item>
+            >
+                Proceedings
+            </x-filament::tabs.item>
+        @endif
     </x-filament::tabs>
 
-    <div x-show="activeTab === 'submissions'">
-        @livewire(App\Panel\Conference\Livewire\SubmissionDOI::class)
-    </div>
-    <div x-show="activeTab === 'proceedings'">
-        @livewire(App\Panel\Conference\Livewire\ProceedingDOI::class)
-    </div>
+    @if($articlesDoiEnabled)
+        <div x-show="activeTab === 'submissions'">
+            @livewire(App\Panel\Conference\Livewire\SubmissionDOI::class)
+        </div>
+    @endif
+
+    @if ($proceedingsDoiEnabled)
+        <div x-show="activeTab === 'proceedings'">
+            @livewire(App\Panel\Conference\Livewire\ProceedingDOI::class)
+        </div>
+    @endif
 
 </x-filament-panels::page>
