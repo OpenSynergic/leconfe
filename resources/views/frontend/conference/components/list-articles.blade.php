85c10b29a0855a41f491b3a0878e7f09bf1ad6a8
faisalwahyudi10 <faisal.piffent.s@gmail.com>
2024-05-15 17:01:27 +0800

    desain-component-article

---
+++ resources/views/frontend/conference/components/list-articles.blade.php
@@ -0,0 +1,174 @@
+<div>
+    @php
+        $authors = [
+            ['name' => 'John Doe', 'url' => 'https://www.arcc-journal.org/index.php/arccjournal'],
+            ['name' => 'Jane Doe', 'url' => 'https://www.arcc-journal.org/index.php/arccjournal'],
+            ['name' => 'Juan Dela Cruz', 'url' => 'https://www.arcc-journal.org/index.php/arccjournal'],
+            // ['name' => 'Maria Dela Cruz', 'url' => 'https://www.arcc-journal.org/index.php/arccjournal'],
+            // ['name' => 'Pedro Dela Cruz', 'url' => 'https://www.arcc-journal.org/index.php/arccjournal'],
+            // ['name' => 'Juan Tamad', 'url' => 'https://www.arcc-journal.org/index.php/arccjournal'],
+            // ['name' => 'Maria Tamad', 'url' => 'https://www.arcc-journal.org/index.php/arccjournal'],
+            // ['name' => 'Pedro Tamad', 'url' => 'https://www.arcc-journal.org/index.php/arccjournal'],
+            // ['name' => 'Juan Dela Cruz', 'url' => 'https://www.arcc-journal.org/index.php/arccjournal'],
+            // ['name' => 'Maria Dela Cruz', 'url' => 'https://www.arcc-journal.org/index.php/arccjournal'],
+            // ['name' => 'Pedro Dela Cruz', 'url' => 'https://www.arcc-journal.org/index.php/arccjournal'],
+        ];
+    @endphp
+    <div class="flex mb-5 space-x-4">
+        <div class="text-xl font-semibold min-w-fit">Articles</div>
+        <hr class="w-full h-px my-auto bg-gray-200 border-0 dark:bg-gray-700">
+    </div>
+    <div class="space-y-5">
+        <div class="grid grid-cols-10 space-x-5">
+            <div class="col-span-2 max-h-36">
+                <img class="w-full h-full" src="https://t4.ftcdn.net/jpg/05/52/69/73/360_F_552697386_oo8dP8v1YrBES6xpWy9JIGZkDLDXHZGj.jpg" alt="">
+            </div>
+            <div class="col-span-8">
+                <div class="mb-3 text-base">
+                    <a href="#" class="font-semibold text-gray-700 hover:text-primary">The Impact of COVID-19 on the Economy</a>
+                    <div class="text-xs text-gray-500">Jama Network Article</div>
+                </div>
+                <div class="grid grid-cols-9 mb-3.5">
+                    <div class="w-full col-span-8 text-xs">
+                        @foreach ($authors as $author)
+                            <a href="{{ $author['url'] }}" class="text-gray-500 hover:text-primary">{{ $author['name'] }}</a>
+                            @if (!$loop->last)
+                                ,
+                            @endif
+                        @endforeach
+                    </div>
+                    <div class="col-span-1 text-xs text-right">
+                        <div class="flex justify-end gap-x-1">
+                            <x-heroicon-o-document-text class="w-4 h-4 my-auto text-gray-500" />
+                            <span>15-20</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex justify-between mb-3.5 text-xs">
+                    <div class="flex space-x-6 text-gray-700">
+                        <div class="flex space-x-1">
+                            <x-heroicon-o-arrow-trending-up class="w-4 h-4 my-auto text-gray-500" />
+                            <span>Abstract Views : 123</span>
+                        </div>
+                        <div class="flex space-x-1">
+                            <x-heroicon-o-arrow-down-tray class="w-4 h-4 my-auto text-gray-500" />
+                            <span>Download : 123</span>
+                        </div>
+                    </div>
+                    <div class="flex space-x-1">
+                        <x-academicon-doi class="w-4 h-4 my-auto text-yellow-400" />
+                        <span>DOI : 10.20473/ijosh.v13i1.2024.13-19</span>
+                    </div>
+                </div>
+                <div class="flex space-x-1.5">
+                    <button type="button" class="px-3 py-1.5 mb-2 text-xs font-medium text-center text-primary border border-primary hover:text-white hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary me-2 dark:border-primary dark:text-primary dark:hover:text-white dark:hover:bg-primary dark:focus:ring-primary">
+                        PDF
+                    </button>
+                    <button type="button" class="px-3 py-1.5 mb-2 text-xs font-medium text-center text-primary border border-primary hover:text-white hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary me-2 dark:border-primary dark:text-primary dark:hover:text-white dark:hover:bg-primary dark:focus:ring-primary">
+                        XML
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="grid grid-cols-10 space-x-5">
+            <div class="col-span-2 max-h-36">
+                <img class="w-full h-full" src="https://www.shutterstock.com/image-photo/there-notebook-word-coo-abbreviation-600nw-2366080151.jpg" alt="">
+            </div>
+            <div class="col-span-8">
+                <div class="mb-3 text-base">
+                    <a href="#" class="font-semibold text-gray-700 hover:text-primary">Econometric Models for Industrial Organization</a>
+                    <div class="text-xs text-gray-500">Jama Network Article</div>
+                </div>
+                <div class="grid grid-cols-9 mb-3.5">
+                    <div class="w-full col-span-8 text-xs">
+                        @foreach ($authors as $author)
+                            <a href="{{ $author['url'] }}" class="text-gray-500 hover:text-primary">{{ $author['name'] }}</a>
+                            {{ !$loop->last ? ', ' : '' }}
+                        @endforeach
+                    </div>
+                    <div class="col-span-1 text-xs text-right">
+                        <div class="flex justify-end gap-x-1">
+                            <x-heroicon-o-document-text class="w-4 h-4 my-auto text-gray-500" />
+                            <span>32-44</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex justify-between mb-3.5 text-xs">
+                    <div class="flex space-x-6 text-gray-700">
+                        <div class="flex space-x-1">
+                            <x-heroicon-o-arrow-trending-up class="w-4 h-4 my-auto text-gray-500" />
+                            <span>Abstract Views : 123</span>
+                        </div>
+                        <div class="flex space-x-1">
+                            <x-heroicon-o-arrow-down-tray class="w-4 h-4 my-auto text-gray-500" />
+                            <span>Download : 123</span>
+                        </div>
+                    </div>
+                    <div class="flex space-x-1">
+                        <x-academicon-doi class="w-4 h-4 my-auto text-yellow-400" />
+                        <span>DOI : 10.20473/ijosh.v13i1.2024.13-19</span>
+                    </div>
+                </div>
+                <div class="flex space-x-1.5">
+                    <button type="button" class="px-3 py-1.5 mb-2 text-xs font-medium text-center text-primary border border-primary hover:text-white hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary me-2 dark:border-primary dark:text-primary dark:hover:text-white dark:hover:bg-primary dark:focus:ring-primary">
+                        PDF
+                    </button>
+                    <button type="button" class="px-3 py-1.5 mb-2 text-xs font-medium text-center text-primary border border-primary hover:text-white hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary me-2 dark:border-primary dark:text-primary dark:hover:text-white dark:hover:bg-primary dark:focus:ring-primary">
+                        XML
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="grid grid-cols-10 space-x-5">
+            <div class="col-span-2 max-h-36">
+                <img class="w-full h-full" src="https://icsblog.s3.ap-south-1.amazonaws.com/blog/wp-content/uploads/2022/09/20151636/Pros-and-Cons-of-Studying-Economics.jpg" alt="">
+            </div>
+            <div class="col-span-8">
+                <div class="mb-3 text-base">
+                    <a href="#" class="font-semibold text-gray-700 hover:text-primary">Financial Mathematics for Actuaries</a>
+                    <div class="text-xs text-gray-500">Jama Network Article</div>
+                </div>
+                <div class="grid grid-cols-9 mb-3.5">
+                    <div class="w-full col-span-8 text-xs">
+                        @foreach ($authors as $author)
+                            <a href="{{ $author['url'] }}" class="text-gray-500 hover:text-primary">{{ $author['name'] }}</a>
+                            @if (!$loop->last)
+                                ,
+                            @endif
+                        @endforeach
+                    </div>
+                    <div class="col-span-1 text-xs text-right">
+                        <div class="flex justify-end gap-x-1">
+                            <x-heroicon-o-document-text class="w-4 h-4 my-auto text-gray-500" />
+                            <span>9-10</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex justify-between mb-3.5 text-xs">
+                    <div class="flex space-x-6 text-gray-700">
+                        <div class="flex space-x-1">
+                            <x-heroicon-o-arrow-trending-up class="w-4 h-4 my-auto text-gray-500" />
+                            <span>Abstract Views : 123</span>
+                        </div>
+                        <div class="flex space-x-1">
+                            <x-heroicon-o-arrow-down-tray class="w-4 h-4 my-auto text-gray-500" />
+                            <span>Download : 123</span>
+                        </div>
+                    </div>
+                    <div class="flex space-x-1">
+                        <x-academicon-doi class="w-4 h-4 my-auto text-yellow-400" />
+                        <span>DOI : 10.20473/ijosh.v13i1.2024.13-19</span>
+                    </div>
+                </div>
+                <div class="flex space-x-1.5">
+                    <button type="button" class="px-3 py-1.5 mb-2 text-xs font-medium text-center text-primary border border-primary hover:text-white hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary me-2 dark:border-primary dark:text-primary dark:hover:text-white dark:hover:bg-primary dark:focus:ring-primary">
+                        PDF
+                    </button>
+                    <button type="button" class="btn btn-primary btn-outline btn-sm">
+                        XML
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
