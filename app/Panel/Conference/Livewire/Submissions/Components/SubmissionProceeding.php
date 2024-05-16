9ef89b2f6b57ccbc64b2b8264cff26f74e64439e
faisalwahyudi10 <faisal.piffent.s@gmail.com>
2024-05-16 15:36:59 +0800

    add-cover-and-pages-input-field-on-submission-proceeding-WIP

--- app/Panel/Conference/Livewire/Submissions/Components/SubmissionProceeding.php
+++ app/Panel/Conference/Livewire/Submissions/Components/SubmissionProceeding.php
@@ -2,11 +2,18 @@
 
 namespace App\Panel\Conference\Livewire\Submissions\Components;
 
+use App\Actions\Submissions\SubmissionUpdateAction;
+use App\Infolists\Components\BladeEntry;
 use App\Models\Proceeding;
 use App\Models\Submission;
+use Filament\Forms\Components\Actions;
+use Filament\Forms\Components\Actions\Action as ActionsAction;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Form;
 use Filament\Infolists\Components\Actions\Action;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Concerns\InteractsWithInfolists;
@@ -19,10 +26,21 @@ class SubmissionProceeding extends \Livewire\Component implements HasForms, HasI
     use InteractsWithForms, InteractsWithInfolists;
 
     public Submission $submission;
+    public array $meta = [];
+    public array $media = [];
 
     public function render()
     {
         return view('panel.conference.livewire.submissions.components.submission-proceeding');
+    }
+
+    public function mount(Submission $submission)
+    {
+        $this->form->fill([
+            'meta' => [
+                'article_pages' => $this->submission->getMeta('article_pages'),
+            ],
+        ]);
     }
 
     public function infolist(Infolist $infolist): Infolist
@@ -32,7 +50,6 @@ class SubmissionProceeding extends \Livewire\Component implements HasForms, HasI
             ->schema([
                 TextEntry::make('id')
                     ->label('Proceeding')
-                    
                     // ->url(fn(Submission $record) => $record->proceeding && route('filament.conference.resources.proceedings.view', ['record' => $record->proceeding]))
                     ->html()
                     ->getStateUsing(function (Submission $record) {
@@ -73,6 +90,39 @@ class SubmissionProceeding extends \Livewire\Component implements HasForms, HasI
                             ])
                             ->action(fn (Submission $record, array $data) => $data['proceeding_id'] ? $record->assignProceeding($data['proceeding_id']) : $record->unassignProceeding())
                     ]),
+                    // BladeEntry::make('form')
+                    //     ->blade('{{ $this->form }}')
             ]);
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                SpatieMediaLibraryFileUpload::make('media.article-cover')
+                    ->label('Cover Image')
+                    ->collection('article-cover')
+                    ->model($this->submission)
+                    ->image()
+                    ->preserveFilenames(),
+                TextInput::make('meta.article_pages')
+                    ->label('Pages')
+                    ->maxWidth('xs')
+                    ->placeholder('e.g. 1-10'),
+            ]);
+    }
+
+    public function submit()
+    {
+        try {
+            $submission = SubmissionUpdateAction::run(
+                $this->form->getState(),
+                $this->submission
+            );
+
+            $this->form->model($submission)->saveRelationships();
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 }
