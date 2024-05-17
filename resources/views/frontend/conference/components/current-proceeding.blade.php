93ef80521660b8c36d342f7444062f4c1416a434
faisalwahyudi10 <faisal.piffent.s@gmail.com>
2024-05-17 17:02:25 +0800

    current-proceeding-component

--- resources/views/frontend/conference/components/current-proceeding.blade.php
+++ resources/views/frontend/conference/components/current-proceeding.blade.php
@@ -1,23 +1,23 @@
-<div class="mb-12">
+@props([
+    'proceeding',
+])
+
+<div>
     <div class="flex mb-5 space-x-4">
         <div class="text-xl font-semibold min-w-fit">Current Proceeding</div>
         <hr class="w-full h-px my-auto bg-gray-200 border-0 dark:bg-gray-700">
     </div>
-    <div class="grid grid-cols-8 gap-x-5">
+    <div class="grid grid-cols-9 gap-x-4">
         <div class="col-span-2 max-w-64">
-            <img src="https://mf-journal.com/public/site/images/admin/modern%20finance%20cover.webp" class="w-full" alt="150">
+            <img src="{{ $proceeding->getFirstMediaUrl('cover') }}" class="w-full" alt="150">
         </div>
-        <div class="col-span-6">
+        <div class="col-span-7">
             <div class="space-y-4">
                 <div class="text-sm font-semibold">
-                    Vol. 10 No. 2 (2022): December
+                    {{ $proceeding->seriesTitle() }}
                 </div>
                 <div class="text-sm text-justify">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, 
-                    eius ipsam at iste nemo odio. Cum obcaecati hic, rerum aliquid soluta nobis voluptatum, 
-                    deserunt deleniti corrupti, commodi quasi fugiat dolor! Lorem ipsum dolor sit amet, 
-                    consectetur adipisicing elit. Maiores unde at, perferendis vero suscipit nam fuga facilis hic dolores aut ut, 
-                    nulla dolore quis ex? Natus consectetur laboriosam deleniti quod!
+                    {{ $proceeding->description }}
                 </div>
                 <div class="text-sm">
                     <span class="font-semibold">Published: </span> 2024-01-01
