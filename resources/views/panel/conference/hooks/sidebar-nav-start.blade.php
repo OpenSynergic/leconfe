581a39896da8293fedfa3496b774064acdf99e3d
faisalwahyudi10 <faisal.piffent.s@gmail.com>
2024-05-31 15:46:33 +0800

    chore: Update sidebar navigation to display switchable conferences

--- resources/views/panel/conference/hooks/sidebar-nav-start.blade.php
+++ resources/views/panel/conference/hooks/sidebar-nav-start.blade.php
@@ -3,70 +3,70 @@
     $currentConference = app()->getCurrentConference();
 
     $conferences = Conference::with(['media'])
-                ->where('path', '!=', $currentConference->path)
-                ->whereHas('conferenceUsers', function ($query) {
-                    $query->where('model_has_roles.model_id', auth()->id());
-                })
-                ->latest();
+        ->where('path', '!=', $currentConference->path)
+        ->whereHas('conferenceUsers', function ($query) {
+            $query->where('model_has_roles.model_id', auth()->id());
+        })
+        ->latest();
 @endphp
 
-<x-filament::dropdown
-    placement="bottom-start"
-    teleport
-    class="-mx-2"
-    id="switch-conference"
-    width="xs"
->
-    <x-slot name="trigger">
-        <button
-            @if (filament()->isSidebarCollapsibleOnDesktop())
-                x-data="{ tooltip: false }"
-                x-effect="
-                    tooltip = $store.sidebar.isOpen
-                        ? false
-                        : {
-                              content: @js($currentConference->name),
-                              placement: document.dir === 'rtl' ? 'left' : 'right',
-                              theme: $store.theme,
-                          }
-                "
-                x-tooltip.html="tooltip"
-            @endif
-            x-bind:disabled="@js(!$conferences->exists())"
-            type="button"
-            class="fi-tenant-menu-trigger group flex w-full items-center justify-center gap-x-3 rounded-lg p-2 text-sm font-medium outline-none transition duration-75 hover:bg-gray-100 focus:bg-gray-100 dark:hover:bg-white/5 dark:focus:bg-white/5"
-        >
-            <x-filament::avatar
-                    :circular="false"
-                    :src="filament()->getTenantAvatarUrl($currentConference)"
-                />
-            <span
+@if($conferences->exists())
+    <x-filament::dropdown
+        placement="bottom-start"
+        teleport
+        class="-mx-2"
+        id="switch-conference"
+        width="xs"
+    >
+        <x-slot name="trigger">
+            <button
                 @if (filament()->isSidebarCollapsibleOnDesktop())
-                    x-show="$store.sidebar.isOpen"
+                    x-data="{ tooltip: false }"
+                    x-effect="
+                        tooltip = $store.sidebar.isOpen
+                            ? false
+                            : {
+                                content: @js($currentConference->name),
+                                placement: document.dir === 'rtl' ? 'left' : 'right',
+                                theme: $store.theme,
+                            }
+                    "
+                    x-tooltip.html="tooltip"
                 @endif
-                class="grid justify-items-start text-start me-auto truncate"
+                type="button"
+                class="fi-tenant-menu-trigger group flex w-full items-center justify-center gap-x-3 rounded-lg p-2 text-sm font-medium outline-none transition duration-75 hover:bg-gray-100 focus:bg-gray-100 dark:hover:bg-white/5 dark:focus:bg-white/5"
             >
-                
+                <x-filament::avatar
+                        :circular="false"
+                        :src="filament()->getTenantAvatarUrl($currentConference)"
+                    />
+                <span
+                    @if (filament()->isSidebarCollapsibleOnDesktop())
+                        x-show="$store.sidebar.isOpen"
+                    @endif
+                    class="grid justify-items-start text-start me-auto truncate"
+                >
+                    
 
-                <span class="text-gray-950 dark:text-white">
-                    {{ $currentConference->name }}
+                    <span class="text-gray-950 dark:text-white">
+                        {{ $currentConference->name }}
+                    </span>
                 </span>
-            </span>
 
-             <x-filament::icon
-                icon="heroicon-m-chevron-down"
-                icon-alias="panels::tenant-menu.toggle-button"
-                class="hidden md:block ms-auto h-5 w-5 shrink-0 text-gray-400 transition duration-75 group-hover:text-gray-500 group-focus:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-400 dark:group-focus:text-gray-400"
-            />
-        </button>
-    </x-slot>
+                <x-filament::icon
+                    icon="heroicon-m-chevron-down"
+                    icon-alias="panels::tenant-menu.toggle-button"
+                    class="hidden md:block ms-auto h-5 w-5 shrink-0 text-gray-400 transition duration-75 group-hover:text-gray-500 group-focus:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-400 dark:group-focus:text-gray-400"
+                />
+            </button>
+        </x-slot>
 
-    @if($conferences->exists())
+        
         <x-filament::dropdown.list>
             <div class="flex w-full items-center gap-2 whitespace-nowrap p-2 text-sm transition-colors duration-75 outline-none font-medium border-b">
                 Switch Conference
             </div>
-            
+                
             @can('Administration:view')
             <x-filament::dropdown.list.item
                 :href="route('filament.administration.home')"
@@ -79,17 +79,54 @@
             @endcan
 
             <div class="max-h-64 overflow-y-scroll">
-            @foreach ($conferences->get() as $conference)
-                <x-filament::dropdown.list.item
-                    :href="$conference->getPanelUrl()"
-                    :icon="filament()->getTenantAvatarUrl($conference)"
-                    tag="a"
-                >
-                    {{ $conference->name }}
-                </x-filament::dropdown.list.item>
-            @endforeach
+                @foreach ($conferences->get() as $conference)
+                    <x-filament::dropdown.list.item
+                        :href="$conference->getPanelUrl()"
+                        :icon="filament()->getTenantAvatarUrl($conference)"
+                        tag="a"
+                    >
+                        {{ $conference->name }}
+                    </x-filament::dropdown.list.item>
+                @endforeach
             </div>
         </x-filament::dropdown.list>
-    @endif
 
-</x-filament::dropdown>
+    </x-filament::dropdown>
+@else
+    <div class="-mx-2">
+        <button
+            @if (filament()->isSidebarCollapsibleOnDesktop())
+                x-data="{ tooltip: false }"
+                x-effect="
+                    tooltip = $store.sidebar.isOpen
+                        ? false
+                        : {
+                            content: @js($currentConference->name),
+                            placement: document.dir === 'rtl' ? 'left' : 'right',
+                            theme: $store.theme,
+                        }
+                "
+                x-tooltip.html="tooltip"
+            @endif
+            type="button"
+            class="fi-tenant-menu-trigger group flex w-full items-center justify-center gap-x-3 rounded-lg p-2 text-sm font-medium outline-none transition duration-75 hover:bg-gray-100 focus:bg-gray-100 dark:hover:bg-white/5 dark:focus:bg-white/5"
+            >
+            <x-filament::avatar
+                    :circular="false"
+                    :src="filament()->getTenantAvatarUrl($currentConference)"
+                />
+            <span
+                @if (filament()->isSidebarCollapsibleOnDesktop())
+                    x-show="$store.sidebar.isOpen"
+                @endif
+                class="grid justify-items-start text-start me-auto truncate"
+            >
+
+
+                <span class="text-gray-950 dark:text-white">
+                    {{ $currentConference->name }}
+                </span>
+            </span>
+        </button>
+    </div>
+@endif
