238593715c2a30681f7bae08db7d11a5dd115ed1
Rahman Ramsi <rahmanramsi19@gmail.com>
2024-06-06 09:55:25 +0800

    use static page from conference

--- app/Frontend/Website/Pages/StaticPage.php
+++ app/Frontend/Website/Pages/StaticPage.php
@@ -2,36 +2,8 @@
 
 namespace App\Frontend\Website\Pages;
 
-use Livewire\Attributes\Title;
-use Illuminate\Support\Facades\Route;
-use Rahmanramsi\LivewirePageGroup\PageGroup;
-use App\Models\StaticPage as StaticPageModel;
-use Rahmanramsi\LivewirePageGroup\Pages\Page;
+use App\Frontend\Conference\Pages\StaticPage as ConferenceStaticPage;
 
-class StaticPage extends Page
+class StaticPage extends ConferenceStaticPage
 {
-    protected static string $view = 'frontend.website.pages.static-page';
-
-    public StaticPageModel $staticPage;
-
-    public function mount()
-    {
-    }
-
-    protected function getViewData(): array
-    {
-        return [
-            //
-        ];
-    }
-
-    public static function routes(PageGroup $pageGroup): void
-    {
-        $slug = static::getSlug();
-
-        Route::get('page/{staticPage:slug}', static::class)
-            ->middleware(static::getRouteMiddleware($pageGroup))
-            ->withoutMiddleware(static::getWithoutRouteMiddleware($pageGroup))
-            ->name((string) str($slug)->replace('/', '.'));
-    }
 }
