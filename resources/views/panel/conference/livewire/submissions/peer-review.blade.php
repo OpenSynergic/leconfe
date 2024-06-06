6d398dc958e111d0e693cf4c74c9ba107e77b459
faisalwahyudi10 <faisal.piffent.s@gmail.com>
2024-06-06 14:50:19 +0800

    fix code style and hidden decision on paper review

--- resources/views/panel/conference/livewire/submissions/peer-review.blade.php
+++ resources/views/panel/conference/livewire/submissions/peer-review.blade.php
@@ -2,6 +2,11 @@
 @use('App\Models\Enums\SubmissionStatus')
 @use('App\Panel\Conference\Livewire\Submissions\Components')
 @use('App\Models\Enums\UserRole')
+
+@php
+    $user = auth()->user();
+@endphp
+
 <div class="space-y-6">
     <div class="grid grid-cols-12 gap-4">
         <div class="space-y-4 col-span-8">
@@ -17,44 +22,56 @@
             {{-- Discussions --}}
             @livewire(Components\Discussions\DiscussionTopic::class, ['submission' => $submission, 'stage' => SubmissionStage::PeerReview, 'lazy' => true])
         </div>
+        
         <div class="self-start sticky z-30 top-24 flex flex-col gap-4 col-span-4" x-data="{ decision:@js($submissionDecision) }">
-            @if ($submission->revision_required)
-                <div class="flex items-center p-4 text-sm border rounded-lg border-warning-400 bg-warning-200 text-warning-600" x-show="!decision" role="alert">
-                    <span class="text-base text-center">
-                        Revisions have been requested.
-                    </span>
-                </div>
-            @endif
-
-            @if($submission->getEditors()->isEmpty())
-                <div class="px-4 py-3.5 text-base text-white rounded-lg border-2 border-primary-700 bg-primary-500">
-                    Assign an editor to enable the editorial decisions for this stage.
-                </div>
-            @else
-                <div class="overflow-hidden rounded-xl bg-white shadow-sm ring-1 ring-gray-950/5 dark:ring-white/10 space-y-3 py-5 px-6" x-show="decision">
-                    <div class="text-base">
-                        {{ $submission->status == SubmissionStatus::Declined ? 'Submission Declined' : 'Submission accepted for review.' }}
+            @if($submission->stage != SubmissionStage::CallforAbstract)    
+                @if ($submission->revision_required)
+                    <div class="flex items-center p-4 text-sm border rounded-lg border-warning-400 bg-warning-200 text-warning-600" x-show="!decision" role="alert">
+                        <span class="text-base text-center">
+                            Revisions have been requested.
+                        </span>
                     </div>
-                    <a href="#" @@click="decision = !decision" class="text-sm text-primary-500 underline">
-                        Change Decision
-                    </a>
-                </div>
-                <div class="flex flex-col gap-4 col-span-4" x-show="!decision">
-                    @can('skipReview', $submission)
-                        {{ $this->skipReviewAction() }}
-                    @endcan
-                    @can('requestRevision', $submission)
-                        {{ $this->requestRevisionAction() }}
-                    @endcan
-                    @can('acceptPaper', $submission)
-                        {{ $this->acceptSubmissionAction() }}
-                    @endcan
-                    @can('declinePaper', $submission)
-                        {{ $this->declineSubmissionAction() }}
-                    @endcan
-                </div>
-            @endif
+                @endif
 
+                @if($submission->getEditors()->isEmpty())
+                    <div class="px-4 py-3.5 text-base text-white rounded-lg border-2 border-primary-700 bg-primary-500">
+                        Assign an editor to enable the editorial decisions for this stage.
+                    </div>
+                @else
+                    <div class="overflow-hidden rounded-xl bg-white shadow-sm ring-1 ring-gray-950/5 dark:ring-white/10 space-y-3 py-5 px-6" x-show="decision">
+                        <div class="text-base">
+                            @if ($submission->status == SubmissionStatus::Declined)
+                                Submission Declined
+                            @elseif ($submission->skipped_review)
+                                Submission skipped for review.
+                            @else
+                                Submission accepted for review.
+                            @endif
+                        </div>
+                        <a href="#" @@click="decision = !decision" class="text-sm text-primary-500 underline">
+                            Change Decision
+                        </a>
+                    </div>
+                    <div @class([
+                        'flex flex-col gap-4 col-span-4',
+                        'hidden' => in_array($submission->status, [SubmissionStatus::Queued, SubmissionStatus::Published]),
+                    ]) x-show="!decision">
+                        @if ($user->can('skipReview', $submission) && ! $submission->skipped_review)
+                            {{ $this->skipReviewAction() }}
+                        @endif
+                        @if ($user->can('requestRevision', $submission) && ! $submission->revision_required)
+                            {{ $this->requestRevisionAction() }}
+                        @endcan
+                        @if ($user->can('acceptPaper', $submission) && ($submission->status != SubmissionStatus::Editing || $submission->skipped_review))
+                            {{ $this->acceptSubmissionAction() }}
+                        @endcan
+                        @if ($user->can('declinePaper', $submission) && ! in_array($submission->status, [SubmissionStatus::Declined]))
+                            {{ $this->declineSubmissionAction() }}
+                        @endcan
+                    </div>
+                @endif
+            @endif
+            
             @livewire(Components\ParticipantList::class, ['submission' => $submission, 'lazy' => true])
         </div>
 
