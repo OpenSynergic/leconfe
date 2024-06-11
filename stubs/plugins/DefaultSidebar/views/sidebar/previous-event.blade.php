f3a9c20c303b4b406eb0244fcf57dda5a98aaf2a
Rahman Ramsi <rahmanramsi19@gmail.com>
2024-06-11 16:10:48 +0800

    dont show previous event block when there's no previous event

--- stubs/plugins/DefaultSidebar/views/sidebar/previous-event.blade.php
+++ stubs/plugins/DefaultSidebar/views/sidebar/previous-event.blade.php
@@ -1,12 +1,14 @@
-<x-website::sidebar  class="sidebar-previous-event space-y-1" :id="$id">
-    <h2 class="text-heading">Previous Event</h2>
-    @foreach ($previousEvents as $event)
-        <div class="card card-compact bg-white border">
-            <div class="card-body">
-                <a href="{{ $event->getHomeUrl() }}" class="flex items-center">
-                    <h2 class="ml-1 text-base text-primary">{{ $event->title }}</h2>
-                </a>
+@if ($previousEvents->isNotEmpty())
+    <x-website::sidebar class="sidebar-previous-event space-y-1" :id="$id">
+        <h2 class="text-heading">Previous Event</h2>
+        @foreach ($previousEvents as $event)
+            <div class="card card-compact bg-white border">
+                <div class="card-body">
+                    <a href="{{ $event->getHomeUrl() }}" class="flex items-center">
+                        <h2 class="ml-1 text-base text-primary">{{ $event->title }}</h2>
+                    </a>
+                </div>
             </div>
-        </div>
-    @endforeach
-</x-website::sidebar>
+        @endforeach
+    </x-website::sidebar>
+@endif
