4c47504671adcf80f4c9763f2920d9bc98f9ac72
Dexter121-xi <evansatria432@gmail.com>
2026-07-18 08:31:48 +0800

    membaiki button

--- resources/views/frontend/scheduledConference/components/layout/simple-with-platform-footer.blade.php
+++ resources/views/frontend/scheduledConference/components/layout/simple-with-platform-footer.blade.php
@@ -16,7 +16,7 @@
         'subheading' => null,
     ])
 
-    <div class="fi-simple-layout">
+    <div class="fi-simple-layout flex min-h-screen flex-col items-center">
         @if (($hasTopbar ?? true) && filament()->auth()->check())
             <div class="fi-simple-layout-header">
                 @if (filament()->hasDatabaseNotifications())
@@ -29,10 +29,10 @@
             </div>
         @endif
 
-        <div class="fi-simple-main-ctn">
+        <div class="fi-simple-main-ctn flex w-full flex-grow items-center justify-center">
             <main
                 @class([
-                    'fi-simple-main',
+                    'fi-simple-main my-16 w-full bg-white px-6 py-12 shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10 sm:rounded-xl sm:px-12',
                     ($maxWidth instanceof Width) ? "fi-width-{$maxWidth->value}" : (is_string($maxWidth) ? (Width::tryFrom($maxWidth) ? "fi-width-{$maxWidth}" : $maxWidth) : 'fi-width-lg'),
                 ])
             >
